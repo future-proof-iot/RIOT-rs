7027159e0f19372e52a0ca7b8037eef43f12ff55
Kaspar Schleiser <kaspar@schleiser.de>
2020-10-15 22:14:25 +0200

    boards: implement config_if scheme

--- boards/src/lib.rs
+++ boards/src/lib.rs
@@ -1,10 +1,21 @@
 #![no_std]
 
-#[cfg(feature = "nrf52840dk")]
-pub use nrf52840dk as board;
+use cfg_if::cfg_if;
 
-#[cfg(feature = "nucleo-f401re")]
-pub use nucleo_f401re as board;
+cfg_if! {
+    if #[cfg(feature = "nrf52840dk")] {
+        pub use nrf52840dk as board;
+    } else if #[cfg(feature = "nucleo-f401re")] {
+        pub use nucleo_f401re as board;
+    } else if #[cfg(feature = "lm3s6965evb")] {
+        pub use lm3s6965evb as board;
+    }
+    else {
+        pub mod board {
+            pub fn init() {}
+        }
+    }
+}
 
 pub fn init() {
     board::init();
