a3657a0ed6065c19e87364d8ed3e7340ddc08515
Kaspar Schleiser <kaspar@schleiser.de>
2020-11-09 13:49:33 +0100

    riot-core-rs: update bindings

--- riot-rs-core/include/msg.h
+++ riot-rs-core/include/msg.h
@@ -3,6 +3,7 @@
 
 #include "riot-rs-core.h"
 
+#define KERNEL_PID_ISR (kernel_pid_t)(-1)
 #define msg_send_int msg_send
 #define msg_try_send msg_send
 
--- riot-rs-core/include/sched.h
+++ riot-rs-core/include/sched.h
@@ -4,5 +4,7 @@
 #include "riot-rs-core.h"
 
 #define KERNEL_PID_UNDEF THREADS_NUMOF
+#define KERNEL_PID_FIRST 0
+#define KERNEL_PID_LAST THREADS_NUMOF
 
 #endif /* SCHED_H */
