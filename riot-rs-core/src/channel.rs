6a33c006121e8d27b4d0a38f17b62446594b8b35
Kaspar Schleiser <kaspar@schleiser.de>
2020-11-17 18:03:44 +0100

    riot-rs-core: channel: implement try_send(), try_recv()

--- riot-rs-core/src/channel.rs
+++ riot-rs-core/src/channel.rs
@@ -29,7 +29,13 @@ where
     state: ChannelState,
 }
 
+#[derive(Debug, PartialEq)]
 pub enum TryRecvError {
+    WouldBlock,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum TrySendError {
     WouldBlock,
 }
 
@@ -44,44 +50,23 @@ where
         }
     }
 
-    fn wait_rx(&mut self) -> T {
-        #[cfg(test)]
-        println!("wait_rx()");
-        if let ChannelState::EmptyWithRxBlocked(waiters) = &mut self.state {
-            let mut res = MaybeUninit::uninit();
-            Thread::current().wait_on(
-                waiters,
-                ThreadState::ChannelRxBlocked(res.as_mut_ptr() as usize),
-            );
-            Thread::yield_higher();
-            Thread::isr_enable_disable();
-            unsafe { res.assume_init() }
-        } else {
-            unreachable!();
-        }
-    }
-
-    pub fn recv(&mut self) -> T {
-        interrupt::free(|_| match &mut self.state {
-            ChannelState::Idle => {
-                #[cfg(test)]
-                println!("recv idle()");
-                self.state = ChannelState::EmptyWithRxBlocked(ThreadList::new());
-                self.wait_rx()
-            }
+    fn try_recv_impl(&mut self) -> Result<T, TryRecvError> {
+        match &mut self.state {
             ChannelState::MessagesAvailable => {
                 #[cfg(test)]
                 println!("recv avail()");
+
                 // unwrap always succeeds
                 let res = self.rb.get().unwrap();
                 if self.rb.is_empty() {
                     self.state = ChannelState::Idle;
                 }
-                res
+                Ok(res)
             }
             ChannelState::FullWithTxBlocked(senders) => {
                 #[cfg(test)]
                 println!("recv full with senders()");
+
                 // unwrap always succeeds
                 let sender = senders.lpop().unwrap();
                 let res = if let Some(msg) = self.rb.get() {
@@ -92,77 +77,189 @@ where
                             self.state = ChannelState::MessagesAvailable;
                         }
                     } else {
-                        panic!("unexpected state in Channel::recv()");
+                        unreachable!();
                     }
-                    msg
+                    Ok(msg)
                 } else {
+                    #[cfg(test)]
+                    println!("recv direct copy()");
                     if let ThreadState::ChannelTxBlocked(ptr) = sender.state {
                         let res = unsafe { *(ptr as *mut T) };
                         sender.set_state(ThreadState::Running);
                         if senders.is_empty() {
                             self.state = ChannelState::MessagesAvailable;
                         }
-                        res
+                        Ok(res)
                     } else {
-                        panic!("unexpected state in Channel::recv()");
+                        unreachable!();
                     }
                 };
                 Thread::yield_higher();
-                Thread::isr_enable_disable();
                 res
             }
-            ChannelState::EmptyWithRxBlocked(_) => {
-                #[cfg(test)]
-                println!("recv empty with waiters()");
-                self.wait_rx()
+            _ => Err(TryRecvError::WouldBlock),
+        }
+    }
+
+    pub fn try_recv(&mut self) -> Result<T, TryRecvError> {
+        interrupt::free(|_| self.try_recv_impl())
+    }
+
+    pub fn recv(&mut self) -> T {
+        interrupt::free(|_| {
+            if let Ok(res) = self.try_recv_impl() {
+                res
+            } else {
+                if let ChannelState::Idle = self.state {
+                    self.state = ChannelState::EmptyWithRxBlocked(ThreadList::new());
+                }
+                let mut res = MaybeUninit::uninit();
+                if let ChannelState::EmptyWithRxBlocked(waiters) = &mut self.state {
+                    Thread::current().wait_on(
+                        waiters,
+                        ThreadState::ChannelRxBlocked(res.as_mut_ptr() as usize),
+                    );
+                } else {
+                    unreachable!();
+                }
+                Thread::yield_higher();
+                Thread::isr_enable_disable();
+                unsafe { res.assume_init() }
             }
         })
     }
 
-    fn wait_tx(&mut self, msg: T) {
-        #[cfg(test)]
-        println!("wait_tx()");
-        if let ChannelState::FullWithTxBlocked(waiters) = &mut self.state {
-            Thread::current().wait_on(
-                waiters,
-                ThreadState::ChannelTxBlocked(&msg as *const T as usize),
-            );
-            Thread::yield_higher();
+    pub fn try_send_impl(&mut self, msg: T) -> Result<(), TrySendError> {
+        if let ChannelState::EmptyWithRxBlocked(waiters) = &mut self.state {
+            #[cfg(test)]
+            println!("tx empty with waiters()");
+            let waiter = waiters.lpop().unwrap();
+            if let ThreadState::ChannelRxBlocked(ptr) = waiter.state {
+                #[cfg(test)]
+                println!("copying message");
+                unsafe { *(ptr as *mut T) = msg };
+                waiter.set_state(ThreadState::Running);
+                Thread::yield_higher();
+                if waiters.is_empty() {
+                    self.state = ChannelState::Idle;
+                }
+            } else {
+                unreachable!();
+            }
+            Ok(())
         } else {
-            unreachable!();
+            if self.rb.put(msg) {
+                #[cfg(test)]
+                println!("try_send() put message ok");
+                if let ChannelState::Idle = self.state {
+                    #[cfg(test)]
+                    println!("setting state to msgavail");
+                    self.state = ChannelState::MessagesAvailable;
+                }
+                Ok(())
+            } else {
+                #[cfg(test)]
+                println!("try_send() wouldblock");
+                Err(TrySendError::WouldBlock)
+            }
         }
     }
 
+    pub fn try_send(&mut self, msg: T) -> Result<(), TrySendError> {
+        interrupt::free(|_| self.try_send_impl(msg))
+    }
+
     pub fn send(&mut self, msg: T) {
-        interrupt::free(|_| match &mut self.state {
-            ChannelState::Idle | ChannelState::MessagesAvailable => {
-                if self.rb.put(msg) {
-                    #[cfg(test)]
-                    println!("send idle avail");
-                    self.state = ChannelState::MessagesAvailable;
-                } else {
-                    #[cfg(test)]
-                    println!("send idle full");
-                    self.state = ChannelState::FullWithTxBlocked(ThreadList::new());
-                    self.wait_tx(msg)
+        interrupt::free(|_| {
+            if let Ok(()) = self.try_send_impl(msg) {
+            } else {
+                match self.state {
+                    ChannelState::FullWithTxBlocked(_) => (),
+                    _ => self.state = ChannelState::FullWithTxBlocked(ThreadList::new()),
                 }
-            }
-            ChannelState::FullWithTxBlocked(_) => self.wait_tx(msg),
-            ChannelState::EmptyWithRxBlocked(waiters) => {
-                #[cfg(test)]
-                println!("tx empty with waiters()");
-                let waiter = waiters.lpop().unwrap();
-                if let ThreadState::ChannelRxBlocked(ptr) = waiter.state {
+                if let ChannelState::FullWithTxBlocked(waiters) = &mut self.state {
                     #[cfg(test)]
-                    println!("copying message");
-                    unsafe { *(ptr as *mut T) = msg };
-                    waiter.set_state(ThreadState::Running);
+                    println!("send() waiting");
+                    Thread::current().wait_on(
+                        waiters,
+                        ThreadState::ChannelTxBlocked(&msg as *const T as usize),
+                    );
                     Thread::yield_higher();
                 } else {
                     unreachable!();
                 }
             }
         })
+    }
+}
+
+pub mod c {
+    use super::Channel;
+    #[allow(non_camel_case_types)]
+    use crate::thread::c::{msg_content_t, msg_t};
+    use crate::thread::{CreateFlags, Msg, Pid, Thread, ThreadFlags};
+    use core::mem::MaybeUninit;
+    use ref_cast::RefCast;
+
+    #[derive(RefCast)]
+    #[repr(transparent)]
+    pub struct mbox_t(Channel<'static, msg_t>);
+
+    pub const MBOX_T_SIZEOF: usize = 20;
+    pub const MBOX_T_ALIGNOF: usize = 4;
+
+    #[test_case]
+    fn test_mbox_const_defines() {
+        assert_eq!(core::mem::size_of::<mbox_t>(), MBOX_T_SIZEOF);
+        assert_eq!(core::mem::align_of::<mbox_t>(), MBOX_T_ALIGNOF);
+    }
+
+    #[no_mangle]
+    pub unsafe extern "C" fn mbox_init(
+        mbox: &'static mut mbox_t,
+        queue: &'static mut msg_t,
+        queue_size: usize,
+    ) {
+        let queue: &'static mut MaybeUninit<msg_t> = core::mem::transmute(queue);
+        let queue = core::slice::from_raw_parts_mut(queue, queue_size);
+        *mbox = mbox_t {
+            0: Channel::new(queue),
+        };
+    }
+
+    #[no_mangle]
+    pub unsafe extern "C" fn mbox_put(mbox: &'static mut mbox_t, msg: &'static mut msg_t) {
+        mbox.0.send(*msg)
+    }
+
+    #[no_mangle]
+    pub unsafe extern "C" fn mbox_get(mbox: &'static mut mbox_t, msg: &'static mut msg_t) {
+        *msg = mbox.0.recv()
+    }
+
+    #[no_mangle]
+    pub unsafe extern "C" fn mbox_try_put(
+        mbox: &'static mut mbox_t,
+        msg: &'static mut msg_t,
+    ) -> bool {
+        match mbox.0.try_send(*msg) {
+            Ok(()) => true,
+            _ => false,
+        }
+    }
+
+    #[no_mangle]
+    pub unsafe extern "C" fn mbox_try_get(
+        mbox: &'static mut mbox_t,
+        msg: &'static mut msg_t,
+    ) -> bool {
+        match mbox.0.try_recv() {
+            Ok(res) => {
+                *msg = res;
+                true
+            }
+            _ => false,
+        }
     }
 }
 
@@ -183,6 +280,8 @@ fn test_channel_lowprio_sender() {
     let mut array: [MaybeUninit<u32>; 4] = unsafe { MaybeUninit::uninit().assume_init() };
     let mut channel = Channel::new(&mut array);
 
+    assert_eq!(channel.try_recv(), Err(TryRecvError::WouldBlock));
+
     unsafe {
         Thread::create(
             &mut STACK,
@@ -193,11 +292,12 @@ fn test_channel_lowprio_sender() {
         );
     }
 
-    println!("recv()");
     assert_eq!(channel.recv(), 0u32);
     assert_eq!(channel.recv(), 1u32);
     assert_eq!(channel.recv(), 2u32);
     assert_eq!(channel.recv(), 3u32);
+
+    assert_eq!(channel.try_recv(), Err(TryRecvError::WouldBlock));
 }
 
 #[test_case]
@@ -217,6 +317,8 @@ fn test_channel_hiprio_sender() {
     let mut array: [MaybeUninit<u32>; 4] = unsafe { MaybeUninit::uninit().assume_init() };
     let mut channel = Channel::new(&mut array);
 
+    assert_eq!(channel.try_recv(), Err(TryRecvError::WouldBlock));
+
     unsafe {
         Thread::create(
             &mut STACK,
@@ -231,4 +333,6 @@ fn test_channel_hiprio_sender() {
     for i in 0..8u32 {
         assert_eq!(channel.recv(), i);
     }
+
+    assert_eq!(channel.try_recv(), Err(TryRecvError::WouldBlock));
 }
