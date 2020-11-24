802436dd4183b32e73300b0a6e0c4e9be23415cc
Kaspar Schleiser <kaspar@schleiser.de>
2020-11-24 22:39:55 +0100

    riot-rs-core: Lock: add some doc

--- riot-rs-core/src/lock.rs
+++ riot-rs-core/src/lock.rs
@@ -1,5 +1,13 @@
 //! A lock is a synchronization primitive that is not owned by a particular
 //! thread when locked.
+//!
+//! When a Lock is unlocked, it can be locked using its `acquire()` method.
+//!
+//! When a Lock is locked, a call to `acquire()` will be blocked until another thread
+//! or ISR unlocks the lock. A call to `release()` will unlock the Lock, unless
+//! another thread is blocking on `acquire()`. In that case, the Lock remains
+//! locked, and one other thread will resume (its call to `acquire()` will return).
+//!
 
 use crate::thread::{Thread, ThreadList, ThreadState};
 use core::cell::UnsafeCell;
@@ -9,24 +17,31 @@ pub struct Lock {
     state: interrupt::Mutex<UnsafeCell<LockState>>,
 }
 
-pub enum LockState {
+enum LockState {
     Unlocked,
     Locked(ThreadList),
 }
 
 impl Lock {
+    /// Create a new, unlocked Lock.
     pub const fn new() -> Lock {
         Lock {
             state: interrupt::Mutex::new(UnsafeCell::new(LockState::Unlocked)),
         }
     }
 
+    /// Create a new, locked Lock.
     pub const fn new_locked() -> Lock {
         Lock {
             state: interrupt::Mutex::new(UnsafeCell::new(LockState::Locked(ThreadList::new()))),
         }
     }
 
+    /// Check the state of a Lock.
+    ///
+    /// returns `true` if the Lock is locked, `false` otherwise.
+    ///
+    /// Note: This method is safe to be called from ISR context.
     pub fn is_locked(&self) -> bool {
         interrupt::free(|cs| match self.get_state_mut(cs) {
             LockState::Unlocked => true,
@@ -34,6 +49,14 @@ impl Lock {
         })
     }
 
+    /// Acquires a Lock. Blocks if it is already locked.
+    ///
+    /// If the Lock was previously unlocked, this method will "lock" it and return.
+    /// If the Lock was previously locked, this method will block until another thread or ISR
+    /// calls the Lock `release()` or `try_release()` method. This function will
+    /// return after that, with the Lock being locked.
+    ///
+    /// Note: _Not allowed to be called from ISR context!_
     pub fn acquire(&self) {
         interrupt::free(|cs| {
             let state = &mut self.get_state_mut(cs);
@@ -46,6 +69,12 @@ impl Lock {
         });
     }
 
+    /// Acquires a Lock it is unlocked.
+    ///
+    /// If the Lock was previously unlocked, this method will "lock" it and return true.
+    /// If the Lock was previously locked, it remains locked, and the function returns false.
+    ///
+    /// Note: This method is safe to be called from ISR context.
     pub fn try_acquire(&self) -> bool {
         return interrupt::free(|cs| {
             let state = &mut self.get_state_mut(cs);
@@ -58,6 +87,15 @@ impl Lock {
         });
     }
 
+    /// Release a Lock.
+    ///
+    /// If the Lock was previously unlocked, will return.
+    /// If the Lock was previously locked, if no other thread is waiting on it,
+    /// the Lock will be unlocked.
+    /// If the Lock was previously locked and there were thread(s) waiting on it,
+    /// the first waiting thread will be woken, and the Lock remains locked.
+    ///
+    /// Note: This method is safe to be called from ISR context.
     pub fn release(&self) {
         interrupt::free(|cs| {
             let state = &mut self.get_state_mut(cs);
@@ -71,7 +109,7 @@ impl Lock {
                     **state = LockState::Unlocked;
                 }
             } else {
-                // what now. panic?
+                // what now. panic? ignore?
             }
         });
     }
