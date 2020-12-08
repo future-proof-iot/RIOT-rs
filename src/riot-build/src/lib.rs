6e7535698c430dab60b6fb5c44f09e02cd4d4f40
Kaspar Schleiser <kaspar@schleiser.de>
2020-12-08 16:08:20 +0100

    Revert "pull out riot-build"
    This reverts commit 85930e813a4258cb93b88817b5b839cf9cd4168e.

---
+++ src/riot-build/src/lib.rs
@@ -0,0 +1,32 @@
+#![no_std]
+
+use linkme::distributed_slice;
+use riot_rs_rt::debug::println;
+use riot_rs_rt::init::INIT_FUNCS;
+
+#[cfg(feature = "riot-rs-core")]
+use riot_rs_core as _;
+
+#[distributed_slice(INIT_FUNCS, 99)]
+fn riot_startup() {
+    extern "C" {
+        pub fn board_init();
+        pub fn kernel_init();
+        //pub fn libc_init();
+    }
+
+    // due to https://github.com/rust-lang/rust/issues/47384,
+    // the board's additions to INIT_FUNCS get silently discarded
+    // if there's not other symbol used from the modules.
+    // Thus explitly link in a dummy.
+    riot_rs_boards::linkme_please();
+
+    println!("riot_build::riot_startup(): launching RIOT startup");
+    unsafe {
+        board_init();
+        //libc_init();
+        kernel_init();
+    }
+}
+
+pub fn linkme_please() {}
