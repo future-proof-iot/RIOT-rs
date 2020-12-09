5552e4f1749816cdc66ddfc0a36da6ab844dc9cd
Kaspar Schleiser <kaspar@schleiser.de>
2020-12-09 14:41:20 +0100

    repair rustflags for "just run" on thumbv7em-none-eabi

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -4,6 +4,6 @@ trap "exit" INT TERM
 trap "kill 0" EXIT
 
 set -x
-openocd -f "board/nordic_nrf52_dk.cfg" -c 'init' -c 'targets' &
+openocd $OPENOCD_ARGS -c 'init' -c 'targets' &
 
 arm-none-eabi-gdb -q -x ${SCRIPTS}/openocd.gdb "$@"
