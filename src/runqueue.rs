6fcbc2bba4204f495ff6e6464113b571e729ff58
Kaspar Schleiser <kaspar@schleiser.de>
2021-06-07 11:48:53 +0200

    make CList support multiple queues
    The previous implementation needed one N_THREADS sized array *per
    priority level*.
    As each thread can only ever be in one queue, one next_idxs array
    is sufficient for all queues.

--- src/runqueue.rs
+++ src/runqueue.rs
@@ -2,47 +2,53 @@ use core::mem;
 use core::sync::atomic::compiler_fence;
 use core::sync::atomic::Ordering;
 
-const USIZE_BITS: usize = mem::size_of::<usize>() * 8;
-
-/// assumptions:
-/// - runqueue numbers are 0..N (exclusive)
-/// - pids are 0..USIZE_BITS (supporting max 32 threads)
-/// - higher runqueue number means higher priority
 use self::clist::CList;
 
-// TODO: use atomics
+/// assumptions:
+/// - runqueue numbers (corresponding priorities) are 0..N_QUEUES (exclusive)
+/// - runqueue numbers fit in usize bits (supporting max 32 priority levels)
+/// - higher runqueue number means higher priority
+/// - pids range from 0..N_THREADS
+/// - N_THREADS is <255 (as u8 is used to store them, but 0xFF is used as
+///   special value)
+///
+///   The current implementation needs an usize for the bit cache,
+///   an [u8; N_QUEUES] array for the list tail indexes
+///   and an [u8; N_THREADS] for the list next indexes.
+
+const USIZE_BITS: usize = mem::size_of::<usize>() * 8;
+
 pub struct RunQueue<const N_QUEUES: usize, const N_THREADS: usize> {
     bitcache: usize,
-    queues: [clist::CList<N_THREADS>; N_QUEUES],
+    queues: clist::CList<N_QUEUES, N_THREADS>,
 }
 
 impl<const N_QUEUES: usize, const N_THREADS: usize> RunQueue<{ N_QUEUES }, { N_THREADS }> {
     pub const fn new() -> RunQueue<{ N_QUEUES }, { N_THREADS }> {
+        // unfortunately we cannot assert!() on the N_QUEUES and N_
         RunQueue {
             bitcache: 0,
-            queues: [CList::new(); N_QUEUES],
+            queues: CList::new(),
         }
     }
 
     /// add thread with pid n to runqueue number rq
     pub fn add(&mut self, n: usize, rq: usize) {
-        debug_assert!(n < USIZE_BITS);
         debug_assert!(n < N_THREADS);
         debug_assert!(rq < N_QUEUES);
         self.bitcache |= 1 << rq;
-        self.queues[rq].push(n as u8);
+        self.queues.push(n as u8, rq as u8);
     }
 
     /// remove thread with pid n from runqueue number rq
     /// @note: this implementation fails if "n" is not the queue's head.
     /// This is fine, RIOT-rs only ever calls del() for the current thread.
     pub fn del(&mut self, n: usize, rq: usize) {
-        debug_assert!(n < USIZE_BITS);
         debug_assert!(rq < N_QUEUES);
-        let popped = self.queues[rq].pop_head();
+        let popped = self.queues.pop_head(rq as u8);
         //
         assert_eq!(popped, Some(n as u8));
-        if self.queues[rq].empty() {
+        if self.queues.empty(rq) {
             self.bitcache &= !(1 << rq);
         }
     }
@@ -59,7 +65,7 @@ impl<const N_QUEUES: usize, const N_THREADS: usize> RunQueue<{ N_QUEUES }, { N_T
         let rq_ffs = Self::ffs(self.bitcache);
         if rq_ffs > 0 {
             let rq = (rq_ffs - 1) as usize;
-            self.queues[rq].peek_head()
+            self.queues.peek_head(rq as u8)
         } else {
             None
         }
@@ -69,24 +75,24 @@ impl<const N_QUEUES: usize, const N_THREADS: usize> RunQueue<{ N_QUEUES }, { N_T
     /// (this is used to "yield" to another thread of *the same* priority)
     pub fn advance(&mut self, rq: usize) {
         debug_assert!(rq < N_QUEUES);
-        self.queues[rq].advance()
+        self.queues.advance(rq as u8)
     }
 }
 
 mod clist {
     #[derive(Debug, Copy, Clone)]
-    pub struct CList<const N: usize> {
-        tail: u8,
-        next_idxs: [u8; N],
+    pub struct CList<const N_QUEUES: usize, const N_THREADS: usize> {
+        tail: [u8; N_QUEUES],
+        next_idxs: [u8; N_THREADS],
     }
 
-    impl<const N: usize> CList<N> {
+    impl<const N_QUEUES: usize, const N_THREADS: usize> CList<N_QUEUES, N_THREADS> {
         pub const fn new() -> Self {
             // TODO: ensure N fits in u8
             // assert!(N<255); is not allowed in const because it could panic
             CList {
-                tail: Self::sentinel(),
-                next_idxs: [Self::sentinel(); N],
+                tail: [Self::sentinel(); N_QUEUES],
+                next_idxs: [Self::sentinel(); N_THREADS],
             }
         }
 
@@ -94,49 +100,49 @@ mod clist {
             0xFF
         }
 
-        pub fn empty(&self) -> bool {
-            self.tail == Self::sentinel()
+        pub fn empty(&self, rq: usize) -> bool {
+            self.tail[rq] == Self::sentinel()
         }
 
-        pub fn push(&mut self, n: u8) {
+        pub fn push(&mut self, n: u8, rq: u8) {
             assert!(n < Self::sentinel());
             if self.next_idxs[n as usize] == Self::sentinel() {
-                if self.tail == Self::sentinel() {
-                    self.tail = n;
+                if self.tail[rq as usize] == Self::sentinel() {
+                    self.tail[rq as usize] = n;
                     self.next_idxs[n as usize] = n;
                 } else {
-                    self.next_idxs[n as usize] = self.next_idxs[self.tail as usize];
-                    self.next_idxs[self.tail as usize] = n;
-                    self.tail = n;
+                    self.next_idxs[n as usize] = self.next_idxs[self.tail[rq as usize] as usize];
+                    self.next_idxs[self.tail[rq as usize] as usize] = n;
+                    self.tail[rq as usize] = n;
                 }
             }
         }
 
-        pub fn pop_head(&mut self) -> Option<u8> {
-            if self.tail == Self::sentinel() {
+        pub fn pop_head(&mut self, rq: u8) -> Option<u8> {
+            if self.tail[rq as usize] == Self::sentinel() {
                 None
             } else {
-                let res = self.next_idxs[self.tail as usize];
-                if res == self.tail {
-                    self.tail = Self::sentinel();
+                let res = self.next_idxs[self.tail[rq as usize] as usize];
+                if res == self.tail[rq as usize] {
+                    self.tail[rq as usize] = Self::sentinel();
                 } else {
-                    self.next_idxs[self.tail as usize] = self.next_idxs[res as usize];
+                    self.next_idxs[self.tail[rq as usize] as usize] = self.next_idxs[res as usize];
                 }
                 Some(res)
             }
         }
 
-        pub fn peek_head(&self) -> Option<u8> {
-            if self.tail == Self::sentinel() {
+        pub fn peek_head(&self, rq: u8) -> Option<u8> {
+            if self.tail[rq as usize] == Self::sentinel() {
                 None
             } else {
-                Some(self.next_idxs[self.tail as usize])
+                Some(self.next_idxs[self.tail[rq as usize] as usize])
             }
         }
 
-        pub fn advance(&mut self) {
-            if self.tail != Self::sentinel() {
-                self.tail = self.next_idxs[self.tail as usize];
+        pub fn advance(&mut self, rq: u8) {
+            if self.tail[rq as usize] != Self::sentinel() {
+                self.tail[rq as usize] = self.next_idxs[self.tail[rq as usize] as usize];
             }
         }
     }
@@ -147,81 +153,81 @@ mod clist {
 
         #[test]
         fn test_clist_basic() {
-            let mut clist: CList<8> = CList::new();
-            assert!(clist.empty());
-            clist.push(0);
-            assert_eq!(clist.pop_head(), Some(0));
-            assert_eq!(clist.pop_head(), None);
+            let mut clist: CList<8, 32> = CList::new();
+            assert!(clist.empty(0));
+            clist.push(0, 0);
+            assert_eq!(clist.pop_head(0), Some(0));
+            assert_eq!(clist.pop_head(0), None);
         }
 
         #[test]
         fn test_clist_push_already_in_list() {
-            let mut clist: CList<8> = CList::new();
-            assert!(clist.empty());
-            clist.push(0);
-            clist.push(0);
-            assert_eq!(clist.pop_head(), Some(0));
-            assert_eq!(clist.pop_head(), None);
-            assert!(clist.empty());
+            let mut clist: CList<8, 32> = CList::new();
+            assert!(clist.empty(0));
+            clist.push(0, 0);
+            clist.push(0, 0);
+            assert_eq!(clist.pop_head(0), Some(0));
+            assert_eq!(clist.pop_head(0), None);
+            assert!(clist.empty(0));
         }
 
         #[test]
         fn test_clist_push_two() {
-            let mut clist: CList<8> = CList::new();
-            assert!(clist.empty());
-            clist.push(0);
-            clist.push(1);
-            assert_eq!(clist.pop_head(), Some(0));
-            assert_eq!(clist.pop_head(), Some(1));
-            assert_eq!(clist.pop_head(), None);
-            assert!(clist.empty());
+            let mut clist: CList<8, 32> = CList::new();
+            assert!(clist.empty(0));
+            clist.push(0, 0);
+            clist.push(1, 0);
+            assert_eq!(clist.pop_head(0), Some(0));
+            assert_eq!(clist.pop_head(0), Some(1));
+            assert_eq!(clist.pop_head(0), None);
+            assert!(clist.empty(0));
         }
 
         #[test]
         fn test_clist_push_all() {
             const N: usize = 255;
-            let mut clist: CList<N> = CList::new();
-            assert!(clist.empty());
+            let mut clist: CList<8, N> = CList::new();
+            assert!(clist.empty(0));
             for i in 0..(N - 1) {
                 println!("pushing {}", i);
-                clist.push(i as u8);
+                clist.push(i as u8, 0);
             }
             for i in 0..(N - 1) {
                 println!("{}", i);
-                assert_eq!(clist.pop_head(), Some(i as u8));
+                assert_eq!(clist.pop_head(0), Some(i as u8));
             }
-            assert_eq!(clist.pop_head(), None);
-            assert!(clist.empty());
+            assert_eq!(clist.pop_head(0), None);
+            assert!(clist.empty(0));
         }
 
         #[test]
         fn test_clist_advance() {
-            let mut clist: CList<8> = CList::new();
-            assert!(clist.empty());
-            clist.push(0);
-            clist.push(1);
-            clist.advance();
-            assert_eq!(clist.pop_head(), Some(1));
-            assert_eq!(clist.pop_head(), Some(0));
-            assert_eq!(clist.pop_head(), None);
-            assert!(clist.empty());
+            let mut clist: CList<8, 32> = CList::new();
+            assert!(clist.empty(0));
+            clist.push(0, 0);
+            clist.push(1, 0);
+            clist.advance(0);
+            assert_eq!(clist.pop_head(0), Some(1));
+            assert_eq!(clist.pop_head(0), Some(0));
+            assert_eq!(clist.pop_head(0), None);
+            assert!(clist.empty(0));
         }
 
         #[test]
         fn test_clist_peek_head() {
-            let mut clist: CList<8> = CList::new();
-            assert!(clist.empty());
-            clist.push(0);
-            clist.push(1);
-            assert_eq!(clist.peek_head(), Some(0));
-            assert_eq!(clist.peek_head(), Some(0));
-            assert_eq!(clist.pop_head(), Some(0));
-            assert_eq!(clist.peek_head(), Some(1));
-            assert_eq!(clist.pop_head(), Some(1));
-            assert_eq!(clist.peek_head(), None);
-            assert_eq!(clist.peek_head(), None);
-            assert_eq!(clist.pop_head(), None);
-            assert!(clist.empty());
+            let mut clist: CList<8, 32> = CList::new();
+            assert!(clist.empty(0));
+            clist.push(0, 0);
+            clist.push(1, 0);
+            assert_eq!(clist.peek_head(0), Some(0));
+            assert_eq!(clist.peek_head(0), Some(0));
+            assert_eq!(clist.pop_head(0), Some(0));
+            assert_eq!(clist.peek_head(0), Some(1));
+            assert_eq!(clist.pop_head(0), Some(1));
+            assert_eq!(clist.peek_head(0), None);
+            assert_eq!(clist.peek_head(0), None);
+            assert_eq!(clist.pop_head(0), None);
+            assert!(clist.empty(0));
         }
     }
 }
