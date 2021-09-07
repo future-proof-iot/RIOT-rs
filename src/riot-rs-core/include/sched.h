a1620e82c8422e89609803fcb4ea5b33d0342f15
Kaspar Schleiser <kaspar@schleiser.de>
2021-09-07 14:35:13 +0200

    src/riot-rs-core: sched.h: use "d" as format for PRIkernel_pid

--- src/riot-rs-core/include/sched.h
+++ src/riot-rs-core/include/sched.h
@@ -1,6 +1,7 @@
 #ifndef SCHED_H
 #define SCHED_H
 
+#include <inttypes.h>
 #include "riot-rs-core.h"
 
 #define KERNEL_PID_UNDEF THREADS_NUMOF
@@ -8,6 +9,6 @@
 #define KERNEL_PID_LAST THREADS_NUMOF
 
 /* TODO: ensure this is correct */
-#define PRIkernel_pid "c"
+#define PRIkernel_pid "d"
 
 #endif /* SCHED_H */
