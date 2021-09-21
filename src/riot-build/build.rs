89a532ff0249e25c3320269abdbb9b2e2a275d73
Kaspar Schleiser <kaspar@schleiser.de>
2021-09-21 16:53:44 +0200

    src/riot-build: make building RIOT with LTO optional

--- src/riot-build/build.rs
+++ src/riot-build/build.rs
@@ -190,5 +190,6 @@ fn main() {
     println!("cargo:rerun-if-env-changed=RIOTBASE");
     println!("cargo:rerun-if-env-changed=USEMODULE");
     println!("cargo:rerun-if-env-changed=CFLAGS_OPT");
+    println!("cargo:rerun-if-env-changed=LTO");
     println!("cargo:rerun-if-changed=Makefile.riotbuild-rs");
 }
