adf7547516fcf9a0f7f0a25cd03c320bc15efaec
Kaspar Schleiser <kaspar@schleiser.de>
2022-03-03 23:25:12 +0100

    src/riot-rs: introduce riot-rs meta subcrate

---
+++ src/riot-rs/src/lib.rs
@@ -0,0 +1,9 @@
+//! riot-rs
+//!
+//! This is a meta-package, pulling in the sub-crates of RIOT-rs.
+
+#![no_std]
+
+pub use riot_build as _;
+pub use riot_rs_core as core;
+pub use riot_rs_rt as _;
