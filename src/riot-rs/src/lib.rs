618fe66787885a6577d0ec32093f970dfddacebf
Kaspar Schleiser <kaspar@schleiser.de>
2023-06-16 21:31:50 +0200

    src/riot-rs: initial embassy support

--- src/riot-rs/src/lib.rs
+++ src/riot-rs/src/lib.rs
@@ -11,3 +11,6 @@ pub use riot_build as _;
 pub use riot_rs_buildinfo as buildinfo;
 pub use riot_rs_core::thread;
 pub use riot_rs_rt as rt;
+
+#[cfg(feature = "embassy")]
+pub use riot_rs_embassy as embassy;
