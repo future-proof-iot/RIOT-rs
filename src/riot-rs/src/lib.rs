678ce41b478867c2ae92bd407110621d554b57f9
Kaspar Schleiser <kaspar@schleiser.de>
2023-07-03 21:41:13 +0200

    src/riot-rs: make riot-build optional, embassy mandatory

--- src/riot-rs/src/lib.rs
+++ src/riot-rs/src/lib.rs
@@ -4,13 +4,7 @@
 
 #![no_std]
 
-// silence warning, using this imports RIOT-c's vector tables
-#[allow(unused_imports)]
-pub use riot_build as _;
-
 pub use riot_rs_buildinfo as buildinfo;
 pub use riot_rs_core::thread;
-pub use riot_rs_rt as rt;
-
-#[cfg(feature = "embassy")]
 pub use riot_rs_embassy as embassy;
+pub use riot_rs_rt as rt;
