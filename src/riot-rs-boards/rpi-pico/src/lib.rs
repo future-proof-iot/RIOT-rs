76b9cc45839a2fa9233bbe4a89fef22516e7296b
Kaspar Schleiser <kaspar@schleiser.de>
2023-07-13 16:37:19 +0200

    riot-rs-boards:rpi-pico: update

--- src/riot-rs-boards/rpi-pico/src/lib.rs
+++ src/riot-rs-boards/rpi-pico/src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#![no_std]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use riot_rs_rt::debug::println;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn init() {
+    println!("rpi-pico::init()");
 }
