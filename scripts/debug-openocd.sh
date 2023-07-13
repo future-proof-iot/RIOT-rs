dd7d25871e244788bf5c71cb9599800421fe402a
Kaspar Schleiser <kaspar@schleiser.de>
2023-07-13 16:42:37 +0200

    scripts/debug-openocd.sh: bump adapter speed

--- scripts/debug-openocd.sh
+++ scripts/debug-openocd.sh
@@ -4,6 +4,6 @@ trap "exit" INT TERM
 trap "kill 0" EXIT
 
 set -x
-openocd $OPENOCD_ARGS -c 'init' -c 'targets' &
+openocd $OPENOCD_ARGS -c 'init' -c 'targets' -c "adapter speed 5000" &
 
 arm-none-eabi-gdb -q -x ${SCRIPTS}/openocd.gdb "$@"
