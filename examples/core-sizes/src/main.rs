27503a3c375efdb82da4d3d0ceff42745a0c1e0a
Kaspar Schleiser <kaspar@schleiser.de>
2023-11-15 09:45:54 +0100

    examples/core-sizes: fix imports & exit code

--- examples/core-sizes/src/main.rs
+++ examples/core-sizes/src/main.rs
@@ -2,18 +2,18 @@
 #![no_std]
 
 use riot_rs as _;
-use riot_rs::rt::debug::{exit, println};
+use riot_rs::rt::debug::{exit, println, EXIT_SUCCESS};
 
 #[no_mangle]
 fn riot_main() {
     println!(
-        "riot_rs::core::lock::Lock: {}",
-        core::mem::size_of::<riot_rs::core::lock::Lock>()
+        "riot_rs::thread::lock::Lock: {}",
+        core::mem::size_of::<riot_rs::thread::lock::Lock>()
     );
     println!(
-        "riot_rs::core::thread::Thread: {}",
-        core::mem::size_of::<riot_rs::core::thread::Thread>()
+        "riot_rs::thread::Thread: {}",
+        core::mem::size_of::<riot_rs::thread::Thread>()
     );
 
-    exit(0);
+    exit(EXIT_SUCCESS);
 }
