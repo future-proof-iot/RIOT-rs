c3dccf1f72bedd6b104a90f4634ce95a874e3de1
Kaspar Schleiser <kaspar@schleiser.de>
2023-11-17 13:48:15 +0100

    riot-rs-embassy: wire up usb_ethernet

--- src/riot-rs-embassy/src/lib.rs
+++ src/riot-rs-embassy/src/lib.rs
@@ -2,7 +2,9 @@
 #![feature(type_alias_impl_trait)]
 #![feature(used_with_arg)]
 
-use embassy_executor::InterruptExecutor;
+use static_cell::make_static;
+
+use embassy_executor::{InterruptExecutor, Spawner};
 
 pub static EXECUTOR: InterruptExecutor = InterruptExecutor::new();
 
@@ -11,7 +13,7 @@ pub mod blocker;
 #[cfg(context = "nrf52")]
 use embassy_nrf as embassy_arch;
 #[cfg(context = "nrf52")]
-use embassy_nrf::interrupt::SWI0_EGU0 as SWI;
+use embassy_nrf::{bind_interrupts, interrupt::SWI0_EGU0 as SWI, peripherals, rng, usb};
 
 #[cfg(context = "rp2040")]
 use embassy_rp as embassy_arch;
@@ -25,6 +27,69 @@ use embassy_arch::interrupt;
 unsafe fn SWI0_EGU0() {
     EXECUTOR.on_interrupt()
 }
+
+use embassy_usb::{Builder, UsbDevice};
+
+//
+// nrf52 usb begin
+#[cfg(context = "nrf52")]
+use embassy_nrf::usb::{vbus_detect::HardwareVbusDetect, Driver};
+
+#[cfg(context = "nrf52")]
+bind_interrupts!(struct Irqs {
+    USBD => usb::InterruptHandler<peripherals::USBD>;
+    POWER_CLOCK => usb::vbus_detect::InterruptHandler;
+    RNG => rng::InterruptHandler<peripherals::RNG>;
+});
+
+#[cfg(context = "nrf52")]
+type UsbDriver = Driver<'static, peripherals::USBD, HardwareVbusDetect>;
+
+// nrf52 usb end
+//
+#[cfg(context = "rp2040")]
+use embassy_rp::{
+    bind_interrupts, peripherals,
+    peripherals::USB,
+    usb::{Driver, InterruptHandler},
+};
+
+// rp2040 usb start
+#[cfg(context = "rp2040")]
+bind_interrupts!(struct Irqs {
+    USBCTRL_IRQ => InterruptHandler<USB>;
+});
+
+#[cfg(context = "rp2040")]
+type UsbDriver = Driver<'static, peripherals::USB>;
+
+//
+// usb common start
+#[embassy_executor::task]
+async fn usb_task(mut device: UsbDevice<'static, UsbDriver>) -> ! {
+    device.run().await
+}
+// usb common end
+//
+
+//
+// usb net begin
+const MTU: usize = 1514;
+
+use embassy_net::{Stack, StackResources};
+use embassy_usb::class::cdc_ncm::embassy_net::{Device, Runner};
+
+#[embassy_executor::task]
+async fn usb_ncm_task(class: Runner<'static, UsbDriver, MTU>) -> ! {
+    class.run().await
+}
+
+#[embassy_executor::task]
+async fn net_task(stack: &'static Stack<Device<'static, MTU>>) -> ! {
+    stack.run().await
+}
+// usb net end
+//
 
 #[cfg(context = "rp2040")]
 #[interrupt]
@@ -46,15 +111,119 @@ unsafe fn SWI_IRQ_1() {
 //     }
 // }
 
+const fn usb_default_config() -> embassy_usb::Config<'static> {
+    // Create embassy-usb Config
+    let mut config = embassy_usb::Config::new(0xc0de, 0xcafe);
+    config.manufacturer = Some("Embassy");
+    config.product = Some("USB-Ethernet example");
+    config.serial_number = Some("12345678");
+    config.max_power = 100;
+    config.max_packet_size_0 = 64;
+
+    // Required for Windows support.
+    config.composite_with_iads = true;
+    config.device_class = 0xEF;
+    config.device_sub_class = 0x02;
+    config.device_protocol = 0x01;
+    config
+}
+
 pub(crate) fn init() {
     riot_rs_rt::debug::println!("riot-rs-embassy::init()");
-    let _p = embassy_arch::init(Default::default());
-
+    let p = embassy_arch::init(Default::default());
     EXECUTOR.start(SWI);
-    // #[cfg(context = "rp2040")]
-    // {
-    //     EXECUTOR.spawner().spawn(embassy_init(_p)).unwrap();
-    // }
+
+    EXECUTOR.spawner().spawn(init_task(p)).unwrap();
+    riot_rs_rt::debug::println!("riot-rs-embassy::init() done");
+}
+
+#[embassy_executor::task]
+async fn init_task(peripherals: embassy_arch::Peripherals) {
+    riot_rs_rt::debug::println!("riot-rs-embassy::init_task()");
+    #[cfg(all(context = "nrf52", feature = "usb"))]
+    {
+        // nrf52840
+        let clock: embassy_nrf::pac::CLOCK = unsafe { core::mem::transmute(()) };
+
+        riot_rs_rt::debug::println!("nrf: enabling ext hfosc...");
+        clock.tasks_hfclkstart.write(|w| unsafe { w.bits(1) });
+        while clock.events_hfclkstarted.read().bits() != 1 {}
+    }
+
+    #[cfg(context = "nrf52")]
+    let usb_driver = { Driver::new(peripherals.USBD, Irqs, HardwareVbusDetect::new(Irqs)) };
+
+    #[cfg(context = "rp2040")]
+    let usb_driver = { Driver::new(peripherals.USB, Irqs) };
+
+    let usb_config = usb_default_config();
+
+    // Create embassy-usb DeviceBuilder using the driver and config.
+    let mut builder = Builder::new(
+        usb_driver,
+        usb_config,
+        &mut make_static!([0; 256])[..],
+        &mut make_static!([0; 256])[..],
+        &mut make_static!([0; 256])[..],
+        &mut make_static!([0; 128])[..],
+        &mut make_static!([0; 128])[..],
+    );
+
+    // Our MAC addr.
+    let our_mac_addr = [0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC];
+    // Host's MAC addr. This is the MAC the host "thinks" its USB-to-ethernet adapter has.
+    let host_mac_addr = [0x88, 0x88, 0x88, 0x88, 0x88, 0x88];
+
+    let usb_cdc_ecm = {
+        use embassy_usb::class::cdc_ncm::{CdcNcmClass, State};
+
+        // Create classes on the builder.
+        CdcNcmClass::new(&mut builder, make_static!(State::new()), host_mac_addr, 64)
+    };
+
+    let spawner = Spawner::for_current_executor().await;
+
+    // Build the builder.
+    let usb = builder.build();
+
+    spawner.spawn(usb_task(usb)).unwrap();
+
+    // let (runner, device) =
+    //     class.into_embassy_net_device::<MTU, 4, 4>(make_static!(NetState::new()), our_mac_addr);
+    // unwrap!(spawner.spawn(usb_ncm_task(runner)));
+    riot_rs_rt::debug::println!("riot-rs-embassy::init_task() done");
+
+    use embassy_usb::class::cdc_ncm::embassy_net::State as NetState;
+    let (runner, device) = usb_cdc_ecm
+        .into_embassy_net_device::<MTU, 4, 4>(make_static!(NetState::new()), our_mac_addr);
+
+    spawner.spawn(usb_ncm_task(runner)).unwrap();
+
+    // network stack
+    //let config = embassy_net::Config::dhcpv4(Default::default());
+    use embassy_net::{Ipv4Address, Ipv4Cidr};
+    let config = embassy_net::Config::ipv4_static(embassy_net::StaticConfigV4 {
+        address: Ipv4Cidr::new(Ipv4Address::new(10, 42, 0, 61), 24),
+        dns_servers: heapless::Vec::new(),
+        gateway: Some(Ipv4Address::new(10, 42, 0, 1)),
+    });
+
+    // Generate random seed
+    // let mut rng = Rng::new(p.RNG, Irqs);
+    // let mut seed = [0; 8];
+    // rng.blocking_fill_bytes(&mut seed);
+    // let seed = u64::from_le_bytes(seed);
+    let seed = 1234u64;
+
+    // Init network stack
+    let stack = &*make_static!(Stack::new(
+        device,
+        config,
+        make_static!(StackResources::<2>::new()),
+        seed
+    ));
+
+    spawner.spawn(net_task(stack)).unwrap();
 }
 
 use linkme::distributed_slice;
