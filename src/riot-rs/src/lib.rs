abde630eccaa59347743e21a2938d1a6374fec63
Kaspar Schleiser <kaspar@schleiser.de>
2024-01-02 20:05:57 +0100

    src/riot-rs: ensure riot-rs-boards gets linked

--- src/riot-rs/src/lib.rs
+++ src/riot-rs/src/lib.rs
@@ -8,3 +8,6 @@ pub use riot_rs_buildinfo as buildinfo;
 pub use riot_rs_core::thread;
 pub use riot_rs_embassy as embassy;
 pub use riot_rs_rt as rt;
+
+// ensure this gets linked
+use riot_rs_boards as _;
