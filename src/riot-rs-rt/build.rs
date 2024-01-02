65c38908ef00895aaadf1e60dabe97bc116c55f0
Kaspar Schleiser <kaspar@schleiser.de>
2024-01-02 19:33:19 +0100

    riot-rs-rt: take in 'linkme.x'

--- src/riot-rs-rt/build.rs
+++ src/riot-rs-rt/build.rs
@@ -1,14 +1,12 @@
 use std::env;
-use std::fs::File;
-use std::io::Write;
 use std::path::PathBuf;
 
 fn main() {
-    // Put the linker script somewhere the linker can find it
+    // Put the linker scripts somewhere the linker can find them
     let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
-    let link_x = include_bytes!("isr_stack.ld.in");
-    let mut f = File::create(out.join("isr_stack.x")).unwrap();
-    f.write_all(link_x).unwrap();
+
+    std::fs::copy("isr_stack.ld.in", out.join("isr_stack.x")).unwrap();
+    std::fs::copy("linkme.x", out.join("linkme.x")).unwrap();
 
     println!("cargo:rustc-link-search={}", out.display());
 }
