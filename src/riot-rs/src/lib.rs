1829b61dbab34508a5264f61c49999703857aef8
ROMemories <romain.fouquet@inria.fr>
2024-01-08 14:49:13 +0100

    feat: define our own `assign_resources` macro
    We need our own peripheral-extraction macro compatible with
    `OptionalPeripherals`.

--- src/riot-rs/src/lib.rs
+++ src/riot-rs/src/lib.rs
@@ -6,7 +6,7 @@
 
 pub use riot_rs_buildinfo as buildinfo;
 pub use riot_rs_core::thread;
-pub use riot_rs_embassy as embassy;
+pub use riot_rs_embassy::{self as embassy, assign_resources};
 pub use riot_rs_rt as rt;
 
 // ensure this gets linked
