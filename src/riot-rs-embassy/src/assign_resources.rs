ac5f17929c03e55f76d007c206f71d1f317d6cd8
ROMemories <romain.fouquet@inria.fr>
2024-01-08 15:28:23 +0100

    docs: document our `assign_resources` macro

--- src/riot-rs-embassy/src/assign_resources.rs
+++ src/riot-rs-embassy/src/assign_resources.rs
@@ -1,3 +1,18 @@
+/// Similarly to
+/// [`assign_resources`](https://docs.rs/assign-resources/latest/assign_resources/macro.assign_resources.html),
+/// this macro allows to extract the specified peripherals from `OptionalPeripherals` for use in an
+/// application.
+///
+/// It makes sense to use this macro multiple times, coupled with conditional compilation (using
+/// the [`cfg`
+/// attribute](https://doc.rust-lang.org/reference/conditional-compilation.html#the-cfg-attribute)),
+/// to define different setups for different boards.
+///
+/// Using the `assign_resources!` macro to define the peripherals to extract will generate another
+/// macro, `split_resources!`, that allows to obtain the requested peripherals where needed (see
+/// the original documentation for details).
+/// `split_resources!` should be provided with an instance of `OptionalPeripherals` to extract
+/// peripherals from.
 // Based on https://github.com/adamgreig/assign-resources/tree/94ad10e2729afdf0fd5a77cd12e68409a982f58a
 // under MIT license
 #[macro_export]
