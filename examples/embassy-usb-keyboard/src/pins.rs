06970483a95aba63bc9e366c7dd27487e8479430
ROMemories <romain.fouquet@inria.fr>
2024-01-12 11:01:59 +0100

    feat(example): add a USB HID keyboard example

---
+++ examples/embassy-usb-keyboard/src/pins.rs
@@ -0,0 +1,12 @@
+use embassy_nrf::peripherals;
+use riot_rs::define_peripherals;
+
+#[cfg(builder = "nrf52840dk")]
+define_peripherals! {
+    Buttons {
+        btn1: P0_11,
+        btn2: P0_12,
+        btn3: P0_24,
+        btn4: P0_25,
+    }
+}
