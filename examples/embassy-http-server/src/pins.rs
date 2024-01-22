caeb3567ef035c4e72bc040fd61b652169e34cbf
ROMemories <romain.fouquet@inria.fr>
2024-01-22 14:19:26 +0100

    feat: simplify `define_peripherals` by removing a level

--- examples/embassy-http-server/src/pins.rs
+++ examples/embassy-http-server/src/pins.rs
@@ -2,12 +2,9 @@ use embassy_nrf::peripherals;
 use riot_rs::define_peripherals;
 
 #[cfg(builder = "nrf52840dk")]
-define_peripherals! {
-    OurPeripherals,
-    buttons: Buttons {
-        btn1: P0_11,
-        btn2: P0_12,
-        btn3: P0_24,
-        btn4: P0_25,
-    }
-}
+define_peripherals!(Buttons {
+    btn1: P0_11,
+    btn2: P0_12,
+    btn3: P0_24,
+    btn4: P0_25,
+});
