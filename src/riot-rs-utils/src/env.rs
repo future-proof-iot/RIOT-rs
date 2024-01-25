cafa295666b66da4d9160c9ec4cf456840cfd51f
Kaspar Schleiser <kaspar@schleiser.de>
2024-01-25 13:03:37 +0100

    riot-rs-utils: introduce riot-rs-utils, move env_utils here

--- src/riot-rs-utils/src/env.rs
+++ src/riot-rs-utils/src/env.rs
@@ -1,13 +1,15 @@
+pub use {const_panic, konst};
+
 macro_rules! define_env_with_default_macro {
     ($macro_name:ident, $parse_fn_name:ident, $output_type_name:literal) => {
         #[macro_export]
         macro_rules! $macro_name {
             ($env_var:literal, $default:expr) => {
                 if let Some(str_value) = option_env!($env_var) {
-                    if let Ok(value) = konst::primitive::$parse_fn_name(str_value) {
+                    if let Ok(value) = $crate::env::konst::primitive::$parse_fn_name(str_value) {
                         value
                     } else {
-                        const_panic::concat_panic!(
+                        $crate::env::const_panic::concat_panic!(
                             "Could not parse environment variable `",
                             $env_var,
                             "=",
@@ -24,4 +26,4 @@ macro_rules! define_env_with_default_macro {
     };
 }
 
-define_env_with_default_macro!(usize_env_or, parse_usize, "a usize");
+define_env_with_default_macro!(usize_from_env_or, parse_usize, "a usize");
