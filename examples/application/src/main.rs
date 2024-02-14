75acef26b84537149112c3ee1d60f26eb6273047
ROMemories <romain.fouquet@inria.fr>
2024-02-14 11:39:13 +0100

    fixup! feat(embassy): simplify the Application trait

--- examples/application/src/main.rs
+++ examples/application/src/main.rs
@@ -3,27 +3,32 @@
 #![feature(type_alias_impl_trait)]
 #![feature(used_with_arg)]
 
-use riot_rs::embassy::{arch, Application, ApplicationInitError, Drivers, InitializationArgs};
+use riot_rs::embassy::{arch, Application, ApplicationError, Drivers};
 
 use riot_rs::rt::debug::println;
 
-struct MyApplication {
-    state: u32, // some state
-}
+struct MyApplication;
 
 impl Application for MyApplication {
-    fn initialize(
-        _peripherals: &mut arch::OptionalPeripherals,
-        _init_args: InitializationArgs,
-    ) -> Result<&dyn Application, ApplicationInitError> {
-        println!("MyApplication::initialize()");
-        Ok(&Self { state: 0 })
+    fn init() -> &'static dyn Application {
+        &Self {}
     }
 
-    fn start(&self, _spawner: embassy_executor::Spawner, _drivers: Drivers) {
-        println!("MyApplication::start()");
-        // ...
+    fn start(
+        &self,
+        _peripherals: &mut arch::OptionalPeripherals,
+        spawner: embassy_executor::Spawner,
+        _drivers: Drivers,
+    ) -> Result<(), ApplicationError> {
+        spawner.spawn(task()).unwrap();
+        Ok(())
     }
+}
+
+#[embassy_executor::task]
+async fn task() {
+    println!("MyApplication::start()");
+    // ...
 }
 
 riot_rs::embassy::riot_initialize!(MyApplication);
