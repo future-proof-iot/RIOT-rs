55f2be868634f1b31d98404e85f72043e4440250
ROMemories <romain.fouquet@inria.fr>
2024-03-01 11:46:28 +0100

    build(boards): allow to have no boards selected
    This allows to run rustdoc and clippy on the top-level `riot-rs` crate.

--- src/riot-rs-boards/src/lib.rs
+++ src/riot-rs-boards/src/lib.rs
@@ -25,15 +25,15 @@ cfg_if! {
     } else if #[cfg(feature = "rpi-pico-w")] {
         // sharing rpi-pico
         pub use rpi_pico as board;
+    } else if #[cfg(feature = "no-boards")] {
+        // Do nothing
     } else {
         compile_error!("no board feature selected");
     }
 }
 
-use linkme::distributed_slice;
-use riot_rs_rt::INIT_FUNCS;
-
-#[distributed_slice(INIT_FUNCS)]
+#[cfg(not(feature = "no-boards"))]
+#[linkme::distributed_slice(riot_rs_rt::INIT_FUNCS)]
 fn init() {
     board::init();
 }
