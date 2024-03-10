60a72a3b52cd2917e1078702627a2631f3a6d3e2
Kaspar Schleiser <kaspar@schleiser.de>
2024-03-10 22:07:41 +0100

    support single thread mode executor, use for esp32

--- src/riot-rs-embassy/src/arch/esp.rs
+++ src/riot-rs-embassy/src/arch/esp.rs
@@ -1,26 +1,12 @@
-use esp_hal::{
-    clock::ClockControl,
-    embassy::{
-        self,
-        executor::{FromCpu1, InterruptExecutor},
-    },
-    prelude::*,
-    timer::TimerGroup,
+use esp_hal::{clock::ClockControl, embassy, prelude::*, timer::TimerGroup};
+
+pub use esp_hal::{
+    embassy::executor::Executor,
+    peripherals::{OptionalPeripherals, Peripherals},
 };
-
-pub(crate) use esp_hal::interrupt::{self};
-pub use esp_hal::peripherals::{OptionalPeripherals, Peripherals};
-
-pub(crate) type Executor = InterruptExecutor<FromCpu1>;
-pub static SWI: () = ();
 
 #[derive(Default)]
 pub struct Config {}
-
-#[interrupt]
-fn FROM_CPU_INTR1() {
-    unsafe { crate::EXECUTOR.on_interrupt() }
-}
 
 pub fn init(_config: Config) -> OptionalPeripherals {
     let mut peripherals = OptionalPeripherals::from(Peripherals::take());
