d56e2617fc59c80c192eeea0f26d7cad161472e1
Kaspar Schleiser <kaspar@schleiser.de>
2024-03-13 14:30:08 +0100

    riot-rs-embassy: switch from `riot-rs-core` to `riot-rs-threads`

--- src/riot-rs-embassy/src/blocker.rs
+++ src/riot-rs-embassy/src/blocker.rs
@@ -1,7 +1,7 @@
 use core::future::Future;
 use core::pin::Pin;
 use core::task::{Context, Poll, RawWaker, RawWakerVTable, Waker};
-use riot_rs_core::thread::{current_pid, flags, flags::ThreadFlags, ThreadId};
+use riot_rs_threads::{current_pid, flags, flags::ThreadFlags, ThreadId};
 
 const THREAD_FLAG_WAKER: ThreadFlags = 1; // TODO: find more appropriate value
 
