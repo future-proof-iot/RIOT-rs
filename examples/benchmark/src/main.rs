e82cf25a268e28b5b714d9e577fb34ea19048f67
ROMemories <romain.fouquet@inria.fr>
2024-03-21 14:59:32 +0100

    feat(examples): add a more interesting benchmark

--- examples/benchmark/src/main.rs
+++ examples/benchmark/src/main.rs
@@ -7,14 +7,20 @@ use riot_rs::debug::println;
 
 #[riot_rs::thread]
 fn main() {
-    match riot_rs::bench::benchmark(10000, || {
-        //
+    match riot_rs::bench::benchmark(1000, || {
+        let mut i = 0;
+
+        // This seems to be measuring branching cost more than anything
+        for _ in 0..1000 {
+            i += 1;
+            core::hint::black_box(i);
+        }
     }) {
         Ok(ticks) => {
             println!("took {} per iteration", ticks);
         }
-        Err(_) => {
-            println!("benchmark returned error");
+        Err(err) => {
+            println!("benchmark returned error: {}", err);
         }
     }
 }
