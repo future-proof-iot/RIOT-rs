052e2bd7d71b1f6f79136a6594eab69341b61221
ROMemories <romain.fouquet@inria.fr>
2024-03-21 14:59:32 +0100

    feat(benchmark-example): print benchmark errors if any

--- examples/benchmark/src/main.rs
+++ examples/benchmark/src/main.rs
@@ -7,14 +7,15 @@ use riot_rs::debug::println;
 
 #[riot_rs::thread(autostart)]
 fn main() {
-    match riot_rs::bench::benchmark(10000, || {
-        //
+    match riot_rs::bench::benchmark(1000, || {
+        // Insert the function to benchmark here.
+        // Consider using `core::hint::black_box()` where necessary.
     }) {
         Ok(ticks) => {
             println!("took {} per iteration", ticks);
         }
-        Err(_) => {
-            println!("benchmark returned error");
+        Err(err) => {
+            println!("benchmark returned error: {}", err);
         }
     }
 }
