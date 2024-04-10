acbdc3ffcb887c3067dce747f0c152c8915fadf3
ROMemories <romain.fouquet@inria.fr>
2024-04-10 13:46:13 +0200

    docs(riot-rs): show the `thread` module in docs
    Fixes #230

--- src/riot-rs/src/lib.rs
+++ src/riot-rs/src/lib.rs
@@ -5,10 +5,9 @@
 //! # Cargo features
 #![doc = document_features::document_features!(feature_label = r#"<span class="stab portability"><code>{feature}</code></span>"#)]
 #![no_std]
-#![feature(doc_cfg)]
+#![feature(doc_auto_cfg)]
 
 #[cfg(feature = "bench")]
-#[doc(cfg(feature = "bench"))]
 #[doc(inline)]
 pub use riot_rs_bench as bench;
 #[doc(inline)]
@@ -20,17 +19,16 @@ pub use riot_rs_embassy as embassy;
 pub use riot_rs_embassy::{define_peripherals, group_peripherals};
 #[doc(inline)]
 pub use riot_rs_rt as rt;
+#[cfg(feature = "threading")]
+#[doc(inline)]
+pub use riot_rs_threads as thread;
 
 // Attribute macros
 pub use riot_rs_macros::config;
 pub use riot_rs_macros::spawner;
 pub use riot_rs_macros::task;
 #[cfg(any(feature = "threading", doc))]
-#[doc(cfg(feature = "threading"))]
 pub use riot_rs_macros::thread;
-
-#[cfg(feature = "threading")]
-pub use riot_rs_threads as thread;
 
 // These are used by proc-macros we provide
 pub use linkme;
