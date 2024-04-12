d39d5d5ca5164ed5c06d53e6ada18e8d06e2c251
ROMemories <romain.fouquet@inria.fr>
2024-04-12 09:53:02 +0200

    WIP: for debugging

--- src/riot-rs-embassy/src/arch/nrf52.rs
+++ src/riot-rs-embassy/src/arch/nrf52.rs
@@ -103,26 +103,28 @@ pub mod internal_temp {
                 let mut temp = self.temp.try_lock().unwrap();
                 *temp = Some(temp::Temp::new(peripheral, Irqs));
 
-                // fn thread_fn() {
-                //     riot_rs_debug::println!("Thread started");
-                //     loop {}
-                //     // loop {
-                //     //     if let Ok(value) = sensor.read() {
-                //     //         // FIXME: use the value set with set_lower_threshold()
-                //     //         if value.value > 22 {
-                //     //             // FIXME: should this be LowerThreshold or HigherThreshold?
-                //     //             let _ = sensor.channel.send(Notification::LowerThreshold);
-                //     //             riot_rs_debug::println!("Test");
-                //     //         }
-                //     //     }
-                //     //
-                //     //     // FIXME: do not busy loop, sleep for some time
-                //     // }
-                // }
+                fn thread_fn() {
+                    // riot_rs_debug::println!("Thread started");
+                    loop {
+                        riot_rs_threads::yield_same();
+                    }
+                    // loop {
+                    //     if let Ok(value) = sensor.read() {
+                    //         // FIXME: use the value set with set_lower_threshold()
+                    //         if value.value > 22 {
+                    //             // FIXME: should this be LowerThreshold or HigherThreshold?
+                    //             let _ = sensor.channel.send(Notification::LowerThreshold);
+                    //             riot_rs_debug::println!("Test");
+                    //         }
+                    //     }
+                    //
+                    //     // FIXME: do not busy loop, sleep for some time
+                    // }
+                }
 
-                // let mut stack = [0u8; 2048_usize];
-                // riot_rs_threads::thread_create_noarg(thread_fn, &mut stack, 1);
-                // riot_rs_debug::println!("Test", );
+                let mut stack = [0u8; 8192_usize];
+                riot_rs_threads::thread_create_noarg(thread_fn, &mut stack, 2);
+                riot_rs_debug::println!("Test",);
 
                 self.enabled.store(true, Ordering::Release);
             }
