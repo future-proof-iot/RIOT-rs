c16dd7c6ffbebca853e8aa9ea34a6cb947ebf23b
ROMemories <romain.fouquet@inria.fr>
2024-04-18 16:38:09 +0200

    refactor(arch): split the `dummy` arch into multiple submodule files
    Split the `dummy` arch to make it more manageable when it keeps growing.

---
+++ src/riot-rs-embassy/src/arch/dummy/executor.rs
@@ -0,0 +1,32 @@
+use embassy_executor::SpawnToken;
+
+use crate::arch;
+
+pub struct Executor;
+
+impl Executor {
+    #[allow(clippy::new_without_default)]
+    pub const fn new() -> Self {
+        // Actually return a value instead of marking it unimplemented like other dummy
+        // functions, because this function is const and is thus run during compilation
+        Self {}
+    }
+
+    pub fn start(&self, _: arch::SWI) {
+        unimplemented!();
+    }
+
+    pub fn spawner(&self) -> Spawner {
+        unimplemented!();
+    }
+}
+
+pub struct Spawner;
+
+impl Spawner {
+    #[allow(clippy::result_unit_err)]
+    pub fn spawn<S>(&self, _token: SpawnToken<S>) -> Result<(), ()> {
+        unimplemented!();
+    }
+    pub fn must_spawn<S>(&self, _token: SpawnToken<S>) {}
+}
---
+++ src/riot-rs-embassy/src/arch/dummy/hwrng.rs
@@ -0,0 +1,5 @@
+use crate::arch;
+
+pub fn construct_rng(_peripherals: &mut arch::OptionalPeripherals) {
+    unimplemented!();
+}
