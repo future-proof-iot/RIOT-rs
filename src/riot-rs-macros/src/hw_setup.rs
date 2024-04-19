e4214f7596c3ef8638b136a128d482e0f81b9d6a
ROMemories <romain.fouquet@inria.fr>
2024-04-19 11:44:48 +0200

    WIP: support conditional in generate_sensor

--- src/riot-rs-macros/src/hw_setup.rs
+++ src/riot-rs-macros/src/hw_setup.rs
@@ -29,46 +29,70 @@ pub fn hw_setup(_args: TokenStream, _item: TokenStream) -> TokenStream {
 }
 
 mod hw_setup {
+    use proc_macro2::TokenStream;
     use quote::{format_ident, quote};
     use riot_rs_hwsetup::Sensor;
-    use proc_macro2::TokenStream;
 
     pub fn generate_sensor(riot_rs_crate: &syn::Ident, sensor_setup: &Sensor) -> TokenStream {
-        let sensor_name = format_ident!("{}", sensor_setup.name());
-        let sensor_ref = format_ident!("{}_REF", sensor_setup.name());
+        let sensor_name = sensor_setup.name();
+
+        let sensor_name_static = format_ident!("{sensor_name}");
+        let sensor_ref = format_ident!("{sensor_name}_REF");
         let sensor_type = crate::utils::parse_type_path(sensor_setup.driver());
 
-        let spawner_fn = format_ident!("{}_init", sensor_setup.name());
+        let spawner_fn = format_ident!("{sensor_name}_init");
 
-        let peripheral_struct = format_ident!("{}Peripherals", sensor_setup.name());
+        let peripheral_struct = format_ident!("{sensor_name}Peripherals");
 
-        // FIXME: impl `on` (context conditional)
-        // FIXME: impl `when` (feature conditional)
+        let on_conds = parse_conditional_list("context", sensor_setup.on());
+        let when_conds = parse_conditional_list("feature", sensor_setup.when());
+
+        // We have to collect the iterator because `cfg_conds` is used multiple times when
+        // expanding
+        let cfg_conds = on_conds.iter().chain(when_conds.iter()).collect::<Vec<_>>();
+        dbg!(&cfg_conds);
+
         // FIXME: codegen the sensor init
 
         let expanded = quote! {
-            // #[cfg(feature = "button-readings")]
-            // {
-                pub static #sensor_name: #sensor_type = #sensor_type::new();
+            // FIXME: does this work with zero cfg_conds?
+            #[cfg(all(#(#cfg_conds),*))]
+            pub static #sensor_name_static: #sensor_type = #sensor_type::new();
 
-                #[#riot_rs_crate::linkme::distributed_slice(#riot_rs_crate::sensors::SENSOR_REFS)]
-                #[linkme(crate = #riot_rs_crate::linkme)]
-                static #sensor_ref: &'static dyn #riot_rs_crate::sensors::Sensor = &#sensor_name;
+            #[cfg(all(#(#cfg_conds),*))]
+            #[#riot_rs_crate::linkme::distributed_slice(#riot_rs_crate::sensors::SENSOR_REFS)]
+            #[linkme(crate = #riot_rs_crate::linkme)]
+            static #sensor_ref: &'static dyn #riot_rs_crate::sensors::Sensor = &#sensor_name_static;
 
-                #[#riot_rs_crate::spawner(autostart, peripherals)]
-                fn #spawner_fn(_spawner: Spawner, peripherals: #peripheral_struct) {
-                //     // FIXME: how to codegen this?
-                //     BUTTON_1.init(#riot_rs_crate::embassy::arch::gpio::Input::new(
-                //         peripherals.p,
-                //         #riot_rs_crate::embassy::arch::gpio::Pull::Up,
-                //     ));
-                }
+            #[cfg(all(#(#cfg_conds),*))]
+            #[#riot_rs_crate::spawner(autostart, peripherals)]
+            fn #spawner_fn(_spawner: Spawner, peripherals: #peripheral_struct) {
+            //     // FIXME: how to codegen this?
+            //     BUTTON_1.init(#riot_rs_crate::embassy::arch::gpio::Input::new(
+            //         peripherals.p,
+            //         #riot_rs_crate::embassy::arch::gpio::Pull::Up,
+            //     ));
+            }
 
-                #riot_rs_crate::define_peripherals!(#peripheral_struct { });
-                // #riot_rs_crate::define_peripherals!(Button1Peripherals { p: P0_11 });
-            // }
+            #[cfg(all(#(#cfg_conds),*))]
+            #riot_rs_crate::define_peripherals!(#peripheral_struct { });
+            // FIXME
+            // #riot_rs_crate::define_peripherals!(Button1Peripherals { p: P0_11 });
         };
 
         TokenStream::from(expanded)
+    }
+
+    fn parse_conditional_list(cfg_attr: &str, conditionals: Option<&str>) -> Vec<TokenStream> {
+        if let Some(on) = conditionals {
+            let context_attr = format_ident!("{cfg_attr}");
+
+            on.split(',')
+                .map(str::trim)
+                .map(|context| quote!(#context_attr = #context))
+                .collect::<Vec<_>>()
+        } else {
+            Vec::new()
+        }
     }
 }
