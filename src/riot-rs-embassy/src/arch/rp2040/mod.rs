e03500f2af8deba556e7c4f289350b7d4c3f91e8
Kaspar Schleiser <kaspar@schleiser.de>
2024-05-03 09:10:51 +0200

    nrf, rp2040: make use of `executor_swi`

--- src/riot-rs-embassy/src/arch/rp2040/mod.rs
+++ src/riot-rs-embassy/src/arch/rp2040/mod.rs
@@ -5,17 +5,9 @@ pub mod usb;
 
 pub(crate) use embassy_executor::InterruptExecutor as Executor;
 pub use embassy_rp::interrupt;
-pub use embassy_rp::interrupt::SWI_IRQ_1 as SWI;
 pub use embassy_rp::{config::Config, peripherals, OptionalPeripherals};
 
-#[interrupt]
-unsafe fn SWI_IRQ_1() {
-    // SAFETY:
-    // - called from ISR
-    // - not called before `start()`, as the interrupt is enabled by `start()`
-    //   itself
-    unsafe { crate::EXECUTOR.on_interrupt() }
-}
+crate::executor_swi!(SWI_IRQ_1);
 
 pub fn init(config: Config) -> OptionalPeripherals {
     // SWI & DMA priority need to match. DMA is hard-coded to P3 by upstream.
