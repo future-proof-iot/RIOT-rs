aaa6cb28c438747c03d67dedeb444a512acc6732
Kaspar Schleiser <kaspar@schleiser.de>
2024-05-03 11:55:43 +0200

    riot-rs-embassy: build.rs: support `CONFIG_SWI`

---
+++ src/riot-rs-embassy/build.rs
@@ -0,0 +1,27 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+
+fn main() {
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+
+    // handle CONFIG_SWI
+    {
+        let dest_path = Path::new(&out_dir).join("swi.rs");
+        if let Ok(var) = env::var("CONFIG_SWI") {
+            fs::write(
+                &dest_path,
+                format!("crate::executor_swi!({});\n", var).as_bytes(),
+            )
+            .expect("write failed");
+        } else {
+            fs::write(
+                &dest_path,
+                b"compile_error!(\"swi.rs included but CONFIG_SWI not set!\");\n",
+            )
+            .expect("write failed");
+        }
+
+        println!("cargo::rerun-if-env-changed=CONFIG_SWI");
+    }
+}
