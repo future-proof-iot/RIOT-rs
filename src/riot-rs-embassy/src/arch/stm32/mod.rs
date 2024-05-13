9edc054bc53f2ac64337dfb12ee7db8d35fe840c
Kaspar Schleiser <kaspar@schleiser.de>
2024-05-13 14:46:34 +0200

    wip

--- src/riot-rs-embassy/src/arch/stm32/mod.rs
+++ src/riot-rs-embassy/src/arch/stm32/mod.rs
@@ -3,12 +3,34 @@ pub use embassy_stm32::{interrupt, peripherals, Config, OptionalPeripherals, Per
 
 use embassy_stm32::interrupt::{InterruptExt, Priority};
 
+#[cfg(feature = "usb")]
+pub mod usb;
+
 include!(concat!(env!("OUT_DIR"), "/swi.rs"));
 
 use riot_rs_debug::println;
 pub fn init(_config: Config) -> OptionalPeripherals {
     let mut config = Config::default();
     {
+        use embassy_stm32::rcc::*;
+        config.rcc.hsi48 = Some(Hsi48Config {
+            sync_from_usb: true,
+        }); // needed for USB
+        config.rcc.sys = Sysclk::PLL1_R;
+        config.rcc.hse = Some(Hse {
+            freq: embassy_stm32::time::Hertz(32000000),
+            mode: HseMode::Oscillator,
+            prescaler: HsePrescaler::DIV1,
+        });
+        config.rcc.pll = Some(Pll {
+            source: PllSource::HSE,
+            prediv: PllPreDiv::DIV2,
+            mul: PllMul::MUL10,
+            divp: None,
+            divq: None,
+            divr: Some(PllRDiv::DIV2), // sysclk 80Mhz (32 / 2 * 10 / 2)
+        });
+        config.rcc.mux.clk48sel = mux::Clk48sel::HSI48;
         //use embassy_stm32::rcc::*;
         // config.rcc.hsi = Some(HSIPrescaler::DIV1);
         // config.rcc.csi = true;
