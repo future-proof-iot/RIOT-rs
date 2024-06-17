dae6d23fe55aefa5d5e71e68dcd1d7a6ca5d86ac
ROMemories <romain.fouquet@inria.fr>
2024-06-17 15:22:42 +0200

    docs(buses): provide additional documentation on drivers

--- src/riot-rs-embassy/src/arch/dummy/i2c.rs
+++ src/riot-rs-embassy/src/arch/dummy/i2c.rs
@@ -1,1 +1,14 @@
+/// Peripheral-agnostic I2C driver implementing [`embedded_hal_async::i2c::I2c`].
+///
+/// The driver instance is meant to be shared between tasks using
+/// [`I2cDevice`](crate::i2c::I2cDevice).
+///
+/// This driver is not architecture-agnostic however, and has as many variants as the MCU has
+/// I2C peripherals, each variant's name being the name of that peripheral.
+/// Each enum variant has an associated value, which is the associated peripheral-specific driver
+/// instance.
+/// The names of peripheral-specific I2C drivers are `I2c$peripheral`, where `$peripheral` is the
+/// name of the I2C peripheral (e.g, driver names can be `I2cI2C0`, `I2cTWISPI0`, or `I2cSERIAL0`).
+/// The constructors of these peripheral-specific drivers depend on the architecture, please see
+/// the examples and tests for reference.
 pub enum I2c {}
--- src/riot-rs-embassy/src/arch/dummy/spi.rs
+++ src/riot-rs-embassy/src/arch/dummy/spi.rs
@@ -1,1 +1,14 @@
+/// Peripheral-agnostic SPI driver implementing [`embedded_hal_async::spi::SpiBus`].
+///
+/// The driver instance is meant to be shared between tasks using
+/// [`SpiDevice`](crate::spi::SpiDevice).
+///
+/// This driver is not architecture-agnostic however, and has as many variants as the MCU has
+/// SPI peripherals, each variant's name being the name of that peripheral.
+/// Each enum variant has an associated value, which is the associated peripheral-specific driver
+/// instance.
+/// The names of peripheral-specific SPI drivers are `Spi$peripheral`, where `$peripheral` is the
+/// name of the SPI peripheral (e.g, driver names can be `SpiSPI2`, `SpiSPI0`, or `SpiSERIAL2`).
+/// The constructors of these peripheral-specific drivers depend on the architecture, please see
+/// the examples and tests for reference.
 pub enum Spi {}
