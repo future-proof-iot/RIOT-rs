48d00ad676de629b7a8c1f66cb7ae4ea0dd9af68
ROMemories <romain.fouquet@inria.fr>
2024-06-19 12:41:47 +0200

    feat(gpio): add preliminary support for outputs

--- src/riot-rs-embassy/src/arch/nrf/gpio.rs
+++ src/riot-rs-embassy/src/arch/nrf/gpio.rs
@@ -1,1 +1,53 @@
-pub use embassy_nrf::gpio::*;
+pub(crate) use embassy_nrf::gpio::Pin;
+
+pub mod output {
+    use embassy_nrf::gpio::{Level, OutputDrive, Pin};
+
+    use crate::gpio::{FromDriveStrength, PinState};
+
+    pub(crate) use embassy_nrf::gpio::Output;
+
+    pub(crate) fn new(
+        pin: impl Pin,
+        initial_state: PinState,
+        drive_strength: DriveStrength,
+    ) -> Output<'static> {
+        let initial_state: bool = initial_state.into();
+        let initial_state = Level::from(initial_state);
+        // TODO: allow to set this as a setter (does not seem possible on nRF, but is on ESP)
+        // TODO: this also depends on the open-drain configuration
+        let output_drive = match drive_strength {
+            DriveStrength::Standard => OutputDrive::Standard,
+            DriveStrength::High => OutputDrive::HighDrive,
+        };
+        Output::new(pin, initial_state, output_drive)
+    }
+
+    #[derive(Copy, Clone, PartialEq, Eq)]
+    pub enum DriveStrength {
+        Standard,
+        High, // Around 10 mA
+    }
+
+    impl Default for DriveStrength {
+        fn default() -> Self {
+            Self::Standard
+        }
+    }
+
+    impl FromDriveStrength for DriveStrength {
+        fn from(drive_strength: crate::gpio::DriveStrength) -> Self {
+            use crate::gpio::DriveStrength::*;
+
+            // ESPs are able to output up to 40 mA, so we somewhat normalize this.
+            match drive_strength {
+                Arch(drive_strength) => drive_strength,
+                Lowest => DriveStrength::Standard,
+                Standard => DriveStrength::default(),
+                Medium => DriveStrength::Standard,
+                High => DriveStrength::High,
+                Highest => DriveStrength::High,
+            }
+        }
+    }
+}
--- src/riot-rs-embassy/src/lib.rs
+++ src/riot-rs-embassy/src/lib.rs
@@ -5,6 +5,7 @@
 #![feature(used_with_arg)]
 
 pub mod define_peripherals;
+pub mod gpio;
 
 #[cfg(context = "cortex-m")]
 pub mod executor_swi;
