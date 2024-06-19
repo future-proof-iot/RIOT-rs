0ed304c646ac2dcd7a08454ca9c31a676abeee25
ROMemories <romain.fouquet@inria.fr>
2024-06-19 12:41:47 +0200

    feat(gpio): add preliminary support for outputs

--- src/riot-rs-embassy/src/arch/rp2040/gpio.rs
+++ src/riot-rs-embassy/src/arch/rp2040/gpio.rs
@@ -1,1 +1,65 @@
-pub use embassy_rp::gpio::*;
+pub(crate) use embassy_rp::gpio::Pin;
+
+pub mod output {
+    use embassy_rp::gpio::{Drive, Level, Pin};
+
+    use crate::gpio::{FromDriveStrength, PinState};
+
+    pub(crate) use embassy_rp::gpio::Output;
+
+    pub(crate) fn new(
+        pin: impl Pin,
+        initial_state: PinState,
+        drive_strength: DriveStrength,
+    ) -> Output<'static> {
+        let initial_state: bool = initial_state.into();
+        let initial_state = Level::from(initial_state);
+        // TODO: allow to set this as a setter (does not seem possible on nRF, but is on ESP)
+        let mut output = Output::new(pin, initial_state);
+        output.set_drive_strength(drive_strength.into());
+        output
+    }
+
+    // We provide our own type because the upstream Drive is not `Copy` and has no `Default` impl.
+    #[derive(Copy, Clone, PartialEq, Eq)]
+    pub enum DriveStrength {
+        _2mA,
+        _4mA,
+        _8mA,
+        _12mA,
+    }
+
+    impl Default for DriveStrength {
+        fn default() -> Self {
+            // Reset value
+            Self::_4mA
+        }
+    }
+
+    impl From<DriveStrength> for Drive {
+        fn from(drive_strength: DriveStrength) -> Self {
+            match drive_strength {
+                DriveStrength::_2mA => Self::_2mA,
+                DriveStrength::_4mA => Self::_4mA,
+                DriveStrength::_8mA => Self::_8mA,
+                DriveStrength::_12mA => Self::_12mA,
+            }
+        }
+    }
+
+    impl FromDriveStrength for DriveStrength {
+        fn from(drive_strength: crate::gpio::DriveStrength) -> Self {
+            use crate::gpio::DriveStrength::*;
+
+            // ESPs are able to output up to 40 mA, so we somewhat normalize this.
+            match drive_strength {
+                Arch(drive_strength) => drive_strength,
+                Lowest => DriveStrength::_2mA,
+                Standard => DriveStrength::default(),
+                Medium => DriveStrength::_8mA,
+                High => DriveStrength::_12mA,
+                Highest => DriveStrength::_12mA,
+            }
+        }
+    }
+}
---
+++ src/riot-rs-embassy/src/gpio.rs
@@ -0,0 +1,110 @@
+use embedded_hal::digital::OutputPin;
+
+use crate::arch::{
+    self,
+    gpio::{
+        output::{DriveStrength as ArchDriveStrength, Output as ArchOutput},
+        Pin as ArchPin,
+    },
+};
+
+pub use embedded_hal::digital::PinState;
+
+pub struct Output {
+    output: ArchOutput<'static>, // FIXME: is this ok to require a 'static pin?
+}
+
+impl Output {
+    pub fn new(pin: impl ArchPin, initial_state: PinState) -> Self {
+        Self::builder(pin, initial_state).build()
+    }
+
+    pub fn builder<P: ArchPin>(pin: P, initial_state: PinState) -> OutputBuilder<P> {
+        OutputBuilder {
+            pin,
+            initial_state,
+            drive_strength: DriveStrength::Standard,
+        }
+    }
+
+    pub fn set_low(&mut self) {
+        // All architectures are infallible.
+        let _ = <Self as OutputPin>::set_low(self);
+    }
+
+    pub fn set_high(&mut self) {
+        // All architectures are infallible.
+        let _ = <Self as OutputPin>::set_high(self);
+    }
+}
+
+pub struct OutputBuilder<P: ArchPin> {
+    pin: P,
+    initial_state: PinState,
+    drive_strength: DriveStrength,
+}
+
+// TODO: should this be marked non_exaustive?
+#[derive(Copy, Clone, PartialEq, Eq)]
+pub enum DriveStrength {
+    Arch(ArchDriveStrength),
+    Lowest,
+    Standard,
+    Medium,
+    High,
+    Highest,
+}
+
+impl Default for DriveStrength {
+    fn default() -> Self {
+        Self::Standard
+    }
+}
+
+// We introduce our own trait instead of using `From` because this conversion is not
+// value-preserving.
+pub trait FromDriveStrength {
+    fn from(drive_strength: DriveStrength) -> ArchDriveStrength;
+}
+
+impl<P: ArchPin> OutputBuilder<P> {
+    pub fn drive_strength(self, drive_strength: DriveStrength) -> Self {
+        Self {
+            drive_strength,
+            ..self
+        }
+    }
+
+    pub fn build(self) -> Output {
+        // TODO: should me move this into `output::new()`s?
+        let drive_strength = <ArchDriveStrength as FromDriveStrength>::from(self.drive_strength);
+
+        let output = arch::gpio::output::new(self.pin, self.initial_state, drive_strength);
+
+        Output { output }
+    }
+}
+
+impl embedded_hal::digital::ErrorType for Output {
+    type Error = <ArchOutput<'static> as embedded_hal::digital::ErrorType>::Error;
+}
+
+impl OutputPin for Output {
+    fn set_low(&mut self) -> Result<(), Self::Error> {
+        <ArchOutput as OutputPin>::set_low(&mut self.output)
+    }
+
+    fn set_high(&mut self) -> Result<(), Self::Error> {
+        <ArchOutput as OutputPin>::set_high(&mut self.output)
+    }
+}
+
+// FIXME
+// Outputs are all stateful outputs on:
+// - embassy-nrf
+// - embassy-rp
+// - esp-hal
+// - embassy-stm32
+// impl StatefulOutputPin for Output {
+//
+// }
