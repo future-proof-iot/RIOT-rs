63dcedbeea464136edbd918e1ca8e45992691a98
Elena Frank <elena.frank@protonmail.com>
2024-06-19 12:18:07 +0200

    fix(embassy): run esp_wifi in a thread if threading is enabled
    This resolves the conflict between the esp_wifi internal scheduler for
    the wifi background task, and the riot-rs-threads scheduling.
    The esp_wifi thread is running at highest prio and frequently woken up
    to run its tasks.

--- src/riot-rs-embassy/src/wifi/esp_wifi.rs
+++ src/riot-rs-embassy/src/wifi/esp_wifi.rs
@@ -1,9 +1,14 @@
-use esp_wifi::{wifi::WifiStaDevice, EspWifiInitialization};
-use once_cell::sync::OnceCell;
-
 use crate::{arch::OptionalPeripherals, Spawner};
-
-use esp_wifi::wifi::{WifiController, WifiDevice};
+use embassy_time::{Duration, Timer};
+use esp_wifi::{
+    wifi::{
+        ClientConfiguration, Configuration, WifiController, WifiDevice, WifiEvent, WifiStaDevice,
+        WifiState,
+    },
+    EspWifiInitialization,
+};
+use once_cell::sync::OnceCell;
+use riot_rs_debug::println;
 
 pub type NetworkDevice = WifiDevice<'static, WifiStaDevice>;
 
@@ -13,6 +18,9 @@ pub type NetworkDevice = WifiDevice<'static, WifiStaDevice>;
 // Using a `once_cell::OnceCell` here for critical-section support, just to be
 // sure.
 pub static WIFI_INIT: OnceCell<EspWifiInitialization> = OnceCell::new();
+
+#[cfg(feature = "threading")]
+pub static WIFI_THREAD_ID: OnceCell<riot_rs_threads::ThreadId> = OnceCell::new();
 
 pub fn init(peripherals: &mut OptionalPeripherals, spawner: Spawner) -> NetworkDevice {
     let wifi = peripherals.WIFI.take().unwrap();
@@ -26,10 +34,11 @@ pub fn init(peripherals: &mut OptionalPeripherals, spawner: Spawner) -> NetworkD
 
 #[embassy_executor::task]
 async fn connection(mut controller: WifiController<'static>) {
-    use riot_rs_debug::println;
-
-    use embassy_time::{Duration, Timer};
-    use esp_wifi::wifi::{ClientConfiguration, Configuration, WifiEvent, WifiState};
+    #[cfg(feature = "threading")]
+    {
+        let thread_id = WIFI_THREAD_ID.get().unwrap();
+        riot_rs_threads::wakeup(*thread_id);
+    }
 
     println!("start connection task");
     println!("Device capabilities: {:?}", controller.get_capabilities());
@@ -62,5 +71,72 @@ async fn connection(mut controller: WifiController<'static>) {
                 Timer::after(Duration::from_millis(5000)).await
             }
         }
+    }
+}
+
+#[cfg(feature = "threading")]
+mod wifi_thread {
+
+    use super::*;
+    #[cfg(context = "esp32c6")]
+    use esp_hal::peripherals::INTPRI as SystemPeripheral;
+    #[cfg(context = "esp32c3")]
+    use esp_hal::peripherals::SYSTEM as SystemPeripheral;
+    use esp_hal::{
+        interrupt,
+        peripherals::{Interrupt, SYSTIMER},
+    };
+
+    // Handle the systimer alarm 0 interrupt, configured in esp-wifi.
+    extern "C" fn systimer_target0_() {
+        unsafe {
+            SYSTIMER::steal()
+                .int_clr()
+                .write(|w| w.target0().clear_bit_by_one())
+        }
+        // Wakeup `esp_wifi_thread`.
+        riot_rs_threads::wakeup(*WIFI_THREAD_ID.get().unwrap());
+    }
+
+    // CPU Interrupt 3 triggers the scheduler in `esp-wifi`.
+    fn yield_to_esp_wifi_scheduler() {
+        unsafe {
+            (&*SystemPeripheral::PTR)
+                .cpu_intr_from_cpu_3()
+                .modify(|_, w| w.cpu_intr_from_cpu_3().set_bit());
+        }
+    }
+
+    /// High priority thread that frequently wakes up to run the esp-wifi
+    /// scheduler.
+    fn esp_wifi_thread() {
+        // Wait until `embassy` was intialized.
+        riot_rs_threads::sleep();
+
+        // Bind the periodic systimer that is configured in esp-wifi to our own handler.
+        unsafe {
+            interrupt::bind_interrupt(
+                Interrupt::SYSTIMER_TARGET0,
+                core::mem::transmute(systimer_target0_ as *const ()),
+            );
+        }
+
+        loop {
+            // Yield to the esp-wifi scheduler tasks, so that they get a chance to run.
+            yield_to_esp_wifi_scheduler();
+            // Sleep until the systimer alarm 0 interrupts again.
+            riot_rs_threads::sleep()
+        }
+    }
+
+    #[linkme::distributed_slice(riot_rs_threads::THREAD_FNS)]
+    fn start_esp_wifi_thread() {
+        let stack = static_cell::make_static!([0u8; 2048]);
+        let id = riot_rs_threads::thread_create_noarg(
+            esp_wifi_thread,
+            stack,
+            riot_rs_threads::SCHED_PRIO_LEVELS as u8 - 1,
+        );
+        WIFI_THREAD_ID.set(id).unwrap();
     }
 }
