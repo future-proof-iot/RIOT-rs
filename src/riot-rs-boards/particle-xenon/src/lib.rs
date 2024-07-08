a3c762913b7f23430e7dee1d4028e5bd64b6b392
Elena Frank <elena.frank@protonmail.com>
2024-07-08 17:37:46 +0200

    feat(boards): use defmt for logging

--- src/riot-rs-boards/particle-xenon/src/lib.rs
+++ src/riot-rs-boards/particle-xenon/src/lib.rs
@@ -1,8 +1,8 @@
 #![no_std]
 
-use riot_rs_debug::println;
+use riot_rs_debug::log::debug;
 
 pub fn init() {
-    println!("particle_xenon::init()");
+    debug!("particle_xenon::init()");
     nrf52::init();
 }
