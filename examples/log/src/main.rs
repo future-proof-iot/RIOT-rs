cf4e6ce5543b3c762c373daadf6397f341d93d1b
Kaspar Schleiser <kaspar@schleiser.de>
2024-07-09 08:50:45 +0200

    feat(examples): add simple log testing application

---
+++ examples/log/src/main.rs
@@ -0,0 +1,15 @@
+#![no_main]
+#![no_std]
+#![feature(type_alias_impl_trait)]
+#![feature(used_with_arg)]
+
+use riot_rs::debug::log::*;
+
+#[riot_rs::task(autostart)]
+async fn main() {
+    defmt::trace!("trace log level enabled");
+    defmt::debug!("debug log level enabled");
+    defmt::info!("info log level enabled");
+    defmt::warn!("warn log level enabled");
+    defmt::error!("error log level enabled (just testing)");
+}
