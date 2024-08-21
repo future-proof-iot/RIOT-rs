99f00f6a77f995bad809201646e0d7363e9f5d50
ROMemories <152802150+ROMemories@users.noreply.github.com>
2024-08-21 17:22:28 +0200

    fixup! feat(i2c): add timeouts on all architectures to prevent hangs

--- src/riot-rs-embassy/src/i2c.rs
+++ src/riot-rs-embassy/src/i2c.rs
@@ -1,12 +1,14 @@
+//! Provides support for the I2C communication bus.
+
 use embassy_embedded_hal::shared_bus::asynch::i2c::I2cDevice as InnerI2cDevice;
 use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
 use embassy_time::Duration;
 
 use crate::arch;
 
-pub use embedded_hal::i2c::NoAcknowledgeSource;
-
-// Architectures are allowed to timeout earlier.
+/// Timeout value for I2C operations.
+///
+/// Architectures are allowed to timeout earlier.
 pub const I2C_TIMEOUT: Duration = Duration::from_millis(100);
 
 // TODO: do we actually need a CriticalSectionRawMutex here?
@@ -86,13 +88,17 @@ macro_rules! impl_async_i2c_for_driver_enum {
 #[allow(unused_imports, reason = "used by arch modules")]
 pub(crate) use impl_async_i2c_for_driver_enum;
 
+/// An I2C error.
 // FIXME: make this non_exhaustive?
-#[derive(Debug)]
+// NOTE(eq): not deriving `Eq` here because it *could* semantically contain floats later.
+#[derive(Debug, Clone, PartialEq)]
 pub enum Error {
     Bus,
     ArbitrationLoss,
+    /// No acknowledgement was received when expected.
     NoAcknowledge(NoAcknowledgeSource),
     Overrun,
+    /// Timeout when attempting to use the bus; most likely the target device is not connected.
     Timeout,
     Other,
 }
@@ -104,10 +110,31 @@ impl embedded_hal::i2c::Error for Error {
         match self {
             Self::Bus => Bus,
             Self::ArbitrationLoss => ArbitrationLoss,
-            Self::NoAcknowledge(ack_source) => NoAcknowledge(*ack_source),
+            Self::NoAcknowledge(ack_source) => NoAcknowledge((*ack_source).into()),
             Self::Overrun => Overrun,
             Self::Timeout => Other, // FIXME: not sure, is this always a lack of ack?
             Self::Other => Other,
+        }
+    }
+}
+
+/// Indicates what protocol step was not acknowledged by the target device.
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum NoAcknowledgeSource {
+    /// The device did not acknowledge its address.
+    Address,
+    /// The device did not acknowledge the data.
+    Data,
+    /// The device did not acknowledge either its address or its data.
+    Unknown,
+}
+
+impl From<NoAcknowledgeSource> for embedded_hal::i2c::NoAcknowledgeSource {
+    fn from(src: NoAcknowledgeSource) -> Self {
+        match src {
+            NoAcknowledgeSource::Address => embedded_hal::i2c::NoAcknowledgeSource::Address,
+            NoAcknowledgeSource::Data => embedded_hal::i2c::NoAcknowledgeSource::Data,
+            NoAcknowledgeSource::Unknown => embedded_hal::i2c::NoAcknowledgeSource::Unknown,
         }
     }
 }
