df12c11a80eaf8b0cf7fe94bebaaf6aebcbd377b
Elena Frank <elena.frank@protonmail.com>
2024-08-26 10:32:18 +0200

    feat(runqueue): add RunQueue iterator

--- src/riot-rs-runqueue/src/lib.rs
+++ src/riot-rs-runqueue/src/lib.rs
@@ -122,4 +122,31 @@ mod tests {
         runqueue.del(ThreadId::new(2));
         assert_eq!(runqueue.get_next(), None);
     }
+
+    #[test]
+    fn iter() {
+        let mut runqueue: RunQueue<8, 32> = RunQueue::new();
+
+        runqueue.add(ThreadId::new(0), RunqueueId::new(0));
+        runqueue.add(ThreadId::new(1), RunqueueId::new(2));
+        runqueue.add(ThreadId::new(2), RunqueueId::new(2));
+        runqueue.add(ThreadId::new(3), RunqueueId::new(3));
+
+        let (head, rq) = runqueue.get_next_with_rq().unwrap();
+        assert_eq!(head, ThreadId::new(3));
+        assert_eq!(rq, RunqueueId::new(3));
+        let mut iter = runqueue.iter_from(head, rq);
+
+        assert_eq!(iter.next(), Some(ThreadId::new(1)));
+        assert_eq!(iter.next(), Some(ThreadId::new(2)));
+        assert_eq!(iter.next(), Some(ThreadId::new(0)));
+
+        assert!(iter.next().is_none());
+        assert!(iter.next().is_none());
+
+        let mut iter2 = runqueue.iter_from(ThreadId::new(1), RunqueueId::new(2));
+        assert_eq!(iter2.next(), Some(ThreadId::new(2)));
+        assert_eq!(iter2.next(), Some(ThreadId::new(0)));
+        assert!(iter2.next().is_none());
+    }
 }
