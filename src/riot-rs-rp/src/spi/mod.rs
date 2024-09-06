d286be706702eff648cb5c6dfaadf6ba720d18b5
ROMemories <152802150+ROMemories@users.noreply.github.com>
2024-09-06 16:14:51 +0200

    feat: add an SPI main abstraction

---
+++ src/riot-rs-rp/src/spi/mod.rs
@@ -0,0 +1,26 @@
+#[doc(alias = "master")]
+pub mod main;
+
+use embassy_rp::spi::{Phase, Polarity};
+use riot_rs_embassy_common::spi::Mode;
+
+fn from_mode(mode: Mode) -> (Polarity, Phase) {
+    match mode {
+        Mode::Mode0 => (Polarity::IdleLow, Phase::CaptureOnFirstTransition),
+        Mode::Mode1 => (Polarity::IdleLow, Phase::CaptureOnSecondTransition),
+        Mode::Mode2 => (Polarity::IdleHigh, Phase::CaptureOnFirstTransition),
+        Mode::Mode3 => (Polarity::IdleHigh, Phase::CaptureOnSecondTransition),
+    }
+}
+
+pub fn init(peripherals: &mut crate::OptionalPeripherals) {
+    // Take all SPI peripherals and do nothing with them.
+    cfg_if::cfg_if! {
+        if #[cfg(context = "rp2040")] {
+            let _ = peripherals.SPI0.take().unwrap();
+            let _ = peripherals.SPI1.take().unwrap();
+        } else {
+            compile_error!("this RP chip is not supported");
+        }
+    }
+}
