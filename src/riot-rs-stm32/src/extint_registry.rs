ROMemories/ariel-os

refactor(embassy)!: extract the STM32 module into its own crate

src/riot-rs-stm32/src/extint_registry.rs
@@ -1,0 +1,101 @@
+use embassy_stm32::{
+    exti::{AnyChannel, Channel},
+    gpio::Pin,
+    peripherals, OptionalPeripherals, Peripheral,
+};
+use portable_atomic::{AtomicBool, AtomicU16, Ordering};
+use riot_rs_shared_types::gpio::input::InterruptError;
+
+pub static EXTINT_REGISTRY: ExtIntRegistry = ExtIntRegistry::new();
+
+pub struct ExtIntRegistry {
+    initialized: AtomicBool,
+    used_interrupt_channels: AtomicU16, // 16 channels
+}
+
+impl ExtIntRegistry {
+    // Collect all channel peripherals so that the registry is the only one managing them.
+    const fn new() -> Self {
+        Self {
+            initialized: AtomicBool::new(false),
+            used_interrupt_channels: AtomicU16::new(0),
+        }
+    }
+
+    pub fn init(&self, peripherals: &mut OptionalPeripherals) {
+        peripherals.EXTI0.take().unwrap();
+        peripherals.EXTI1.take().unwrap();
+        peripherals.EXTI2.take().unwrap();
+        peripherals.EXTI3.take().unwrap();
+        peripherals.EXTI4.take().unwrap();
+        peripherals.EXTI5.take().unwrap();
+        peripherals.EXTI6.take().unwrap();
+        peripherals.EXTI7.take().unwrap();
+        peripherals.EXTI8.take().unwrap();
+        peripherals.EXTI9.take().unwrap();
+        peripherals.EXTI10.take().unwrap();
+        peripherals.EXTI11.take().unwrap();
+        peripherals.EXTI12.take().unwrap();
+        peripherals.EXTI13.take().unwrap();
+        peripherals.EXTI14.take().unwrap();
+        peripherals.EXTI15.take().unwrap();
+
+        self.initialized.store(true, Ordering::Release);
+
+        // Do nothing else, just consume the peripherals: they are ours now!
+    }
+
+    pub fn get_interrupt_channel_for_pin<P: Peripheral<P = T>, T: Pin>(
+        &self,
+        pin: P,
+    ) -> Result<AnyChannel, InterruptError> {
+        // Make sure that the interrupt channels have been captured during initialization.
+        assert!(self.initialized.load(Ordering::Acquire));
+
+        let pin = pin.into_ref().map_into();
+        let pin_number = pin.pin();
+
+        // As interrupt channels are mutually exclusive between ports (ie., if channel i has
+        // been bound for pin i of a port, it cannot be used for pin i of another port), we
+        // only check the pin number.
+        // NOTE(ordering): since setting a bit is an idempotent operation, and since we do not
+        // allow clearing them, the ordering does not matter.
+        let was_used = self
+            .used_interrupt_channels
+            .bit_set(pin_number.into(), Ordering::Relaxed);
+
+        if was_used {
+            return Err(InterruptError::IntChannelAlreadyUsed);
+        }
+
+        // They are the same
+        let ch_number = pin_number;
+
+        // NOTE(embassy): ideally we would be using `T::ExtiChannel::steal()` instead of this
+        // match, but Embassy does not provide this.
+        // SAFETY: this function enforces that the same channel cannot be obtained twice,
+        // making sure multiple instances are not used at the same time as the mandatory
+        // `init()` method has collected all channel peripherals beforehand.
+        let ch = match ch_number {
+            0 => unsafe { peripherals::EXTI0::steal() }.degrade(),
+            1 => unsafe { peripherals::EXTI1::steal() }.degrade(),
+            2 => unsafe { peripherals::EXTI2::steal() }.degrade(),
+            3 => unsafe { peripherals::EXTI3::steal() }.degrade(),
+            4 => unsafe { peripherals::EXTI4::steal() }.degrade(),
+            5 => unsafe { peripherals::EXTI5::steal() }.degrade(),
+            6 => unsafe { peripherals::EXTI6::steal() }.degrade(),
+            7 => unsafe { peripherals::EXTI7::steal() }.degrade(),
+            8 => unsafe { peripherals::EXTI8::steal() }.degrade(),
+            9 => unsafe { peripherals::EXTI9::steal() }.degrade(),
+            10 => unsafe { peripherals::EXTI10::steal() }.degrade(),
+            11 => unsafe { peripherals::EXTI11::steal() }.degrade(),
+            12 => unsafe { peripherals::EXTI12::steal() }.degrade(),
+            13 => unsafe { peripherals::EXTI13::steal() }.degrade(),
+            14 => unsafe { peripherals::EXTI14::steal() }.degrade(),
+            15 => unsafe { peripherals::EXTI15::steal() }.degrade(),
+            _ => unreachable!(),
+        };
+
+        Ok(ch)
+    }
+}

src/riot-rs-stm32/src/gpio.rs
@@ -1,0 +1,165 @@
+pub trait IntoLevel {
+    fn into(level: Self) -> riot_rs_shared_types::gpio::Level;
+}
+
+pub mod input {
+    use embassy_stm32::{
+        gpio::{Level, Pull},
+        Peripheral,
+    };
+
+    pub use embassy_stm32::gpio::{Input, Pin as InputPin};
+
+    #[cfg(feature = "external-interrupts")]
+    pub use embassy_stm32::exti::ExtiInput as IntEnabledInput;
+
+    pub const SCHMITT_TRIGGER_CONFIGURABLE: bool = false;
+
+    pub fn new(
+        pin: impl Peripheral<P: InputPin> + 'static,
+        pull: riot_rs_shared_types::gpio::Pull,
+        _schmitt_trigger: bool, // Not supported by this architecture
+    ) -> Result<Input<'static>, riot_rs_shared_types::gpio::input::Error> {
+        // let pull = Pull::from(pull);
+        let pull = from_pull(pull);
+        Ok(Input::new(pin, pull))
+    }
+
+    #[cfg(feature = "external-interrupts")]
+    pub fn new_int_enabled<P: Peripheral<P = T> + 'static, T: InputPin>(
+        pin: P,
+        pull: riot_rs_shared_types::gpio::Pull,
+        _schmitt_trigger: bool, // Not supported by this architecture
+    ) -> Result<IntEnabledInput<'static>, riot_rs_shared_types::gpio::input::Error> {
+        // let pull = Pull::from(pull);
+        let pull = from_pull(pull);
+        let mut pin = pin.into_ref();
+        let ch = crate::extint_registry::EXTINT_REGISTRY.get_interrupt_channel_for_pin(&mut pin)?;
+        let pin = pin.into_ref().map_into();
+        Ok(IntEnabledInput::new(pin, ch, pull))
+    }
+
+    impl crate::gpio::IntoLevel for Level {
+        fn into(level: Self) -> riot_rs_shared_types::gpio::Level {
+            match level {
+                Level::Low => riot_rs_shared_types::gpio::Level::Low,
+                Level::High => riot_rs_shared_types::gpio::Level::High,
+            }
+        }
+    }
+
+    fn from_pull(pull: riot_rs_shared_types::gpio::Pull) -> Pull {
+        match pull {
+            riot_rs_shared_types::gpio::Pull::None => Pull::None,
+            riot_rs_shared_types::gpio::Pull::Up => Pull::Up,
+            riot_rs_shared_types::gpio::Pull::Down => Pull::Down,
+        }
+    }
+
+    // impl From<riot_rs_shared_types::gpio::Pull> for Pull {
+    //     fn from(pull: riot_rs_shared_types::gpio::Pull) -> Self {
+    //         match pull {
+    //             riot_rs_shared_types::gpio::Pull::None => Pull::None,
+    //             riot_rs_shared_types::gpio::Pull::Up => Pull::Up,
+    //             riot_rs_shared_types::gpio::Pull::Down => Pull::Down,
+    //         }
+    //     }
+    // }
+    //
+    // impl From<Level> for riot_rs_shared_types::gpio::Level {
+    //     fn from(level: Level) -> Self {
+    //         match level {
+    //             Level::Low => riot_rs_shared_types::gpio::Level::Low,
+    //             Level::High => riot_rs_shared_types::gpio::Level::High,
+    //         }
+    //     }
+    // }
+}
+
+pub mod output {
+    use embassy_stm32::{
+        gpio::{Level, Speed as StmSpeed},
+        Peripheral,
+    };
+
+    use riot_rs_shared_types::gpio::{FromDriveStrength, FromSpeed};
+
+    pub use embassy_stm32::gpio::{Output, Pin as OutputPin};
+
+    pub const DRIVE_STRENGTH_CONFIGURABLE: bool = false;
+    pub const SPEED_CONFIGURABLE: bool = true;
+
+    pub fn new(
+        pin: impl Peripheral<P: OutputPin> + 'static,
+        initial_level: riot_rs_shared_types::gpio::Level,
+        _drive_strength: DriveStrength, // Not supported by this architecture
+        speed: Speed,
+    ) -> Output<'static> {
+        let initial_level = match initial_level {
+            riot_rs_shared_types::gpio::Level::Low => Level::Low,
+            riot_rs_shared_types::gpio::Level::High => Level::High,
+        };
+        Output::new(pin, initial_level, speed.into())
+    }
+
+    // riot_rs_shared_types::impl_from_level!(Level);
+
+    #[derive(Copy, Clone, PartialEq, Eq)]
+    pub enum DriveStrength {
+        UnsupportedByArchitecture,
+    }
+
+    impl Default for DriveStrength {
+        fn default() -> Self {
+            Self::UnsupportedByArchitecture
+        }
+    }
+
+    impl FromDriveStrength for DriveStrength {
+        fn from(drive_strength: riot_rs_shared_types::gpio::DriveStrength<Self>) -> Self {
+            use riot_rs_shared_types::gpio::DriveStrength::*;
+
+            match drive_strength {
+                Arch(drive_strength) => drive_strength,
+                Lowest => DriveStrength::UnsupportedByArchitecture,
+                Standard => DriveStrength::default(),
+                Medium => DriveStrength::UnsupportedByArchitecture,
+                High => DriveStrength::UnsupportedByArchitecture,
+                Highest => DriveStrength::UnsupportedByArchitecture,
+            }
+        }
+    }
+
+    #[derive(Copy, Clone, PartialEq, Eq)]
+    pub enum Speed {
+        Low,
+        Medium,
+        High,
+        VeryHigh,
+    }
+
+    impl From<Speed> for StmSpeed {
+        fn from(speed: Speed) -> Self {
+            match speed {
+                Speed::Low => StmSpeed::Low,
+                Speed::Medium => StmSpeed::Medium,
+                Speed::High => StmSpeed::High,
+                Speed::VeryHigh => StmSpeed::VeryHigh,
+            }
+        }
+    }
+
+    impl FromSpeed for Speed {
+        fn from(speed: riot_rs_shared_types::gpio::Speed<Self>) -> Self {
+            use riot_rs_shared_types::gpio::Speed::*;
+
+            match speed {
+                Arch(speed) => speed,
+                Low => Speed::Low,
+                Medium => Speed::Medium,
+                High => Speed::High,
+                VeryHigh => Speed::VeryHigh,
+            }
+        }
+    }
+}

src/riot-rs-stm32/src/usb_synopsis_otg.rs
@@ -1,6 +1,5 @@
 use embassy_stm32::{bind_interrupts, peripherals, usb, usb::Driver};
-
-use crate::arch;
+use static_cell::make_static;
 
 bind_interrupts!(struct Irqs {
     OTG_FS => usb::InterruptHandler<peripherals::USB_OTG_FS>;
@@ -8,7 +7,7 @@ bind_interrupts!(struct Irqs {
 
 pub type UsbDriver = Driver<'static, peripherals::USB_OTG_FS>;
 
-pub fn driver(peripherals: &mut arch::OptionalPeripherals) -> UsbDriver {
+pub fn driver(peripherals: &mut crate::OptionalPeripherals) -> UsbDriver {
     let usb = peripherals.USB_OTG_FS.take().unwrap();
     let dp = peripherals.PA12.take().unwrap();
     let dm = peripherals.PA11.take().unwrap();
@@ -17,7 +16,7 @@ pub fn driver(peripherals: &mut arch::OptionalPeripherals) -> UsbDriver {
     // "An internal buffer used to temporarily store received packets.
     // Must be large enough to fit all OUT endpoint max packet sizes.
     // Endpoint allocation will fail if it is too small."
-    let ep_out_buffer = crate::make_static!([0u8; 256]);
+    let ep_out_buffer = make_static!([0u8; 256]);
     let mut config = embassy_stm32::usb::Config::default();
 
     // Enable vbus_detection
@@ -32,7 +31,7 @@ pub fn driver(peripherals: &mut arch::OptionalPeripherals) -> UsbDriver {
     #[cfg(feature = "executor-interrupt")]
     {
         use embassy_stm32::interrupt::{InterruptExt, Priority};
-        crate::arch::SWI.set_priority(Priority::P1);
+        crate::SWI.set_priority(Priority::P1);
         embassy_stm32::interrupt::OTG_FS.set_priority(Priority::P0);
     }
 

future-proof-iot/RIOT-rs

src/riot-rs-stm32/src/extint_registry.rs
@@ -1,0 +1,101 @@
+use embassy_stm32::{
+    exti::{AnyChannel, Channel},
+    gpio::Pin,
+    peripherals, OptionalPeripherals, Peripheral,
+};
+use portable_atomic::{AtomicBool, AtomicU16, Ordering};
+use riot_rs_embassy_common::gpio::input::InterruptError;
+
+pub static EXTINT_REGISTRY: ExtIntRegistry = ExtIntRegistry::new();
+
+pub struct ExtIntRegistry {
+    initialized: AtomicBool,
+    used_interrupt_channels: AtomicU16, // 16 channels
+}
+
+impl ExtIntRegistry {
+    // Collect all channel peripherals so that the registry is the only one managing them.
+    const fn new() -> Self {
+        Self {
+            initialized: AtomicBool::new(false),
+            used_interrupt_channels: AtomicU16::new(0),
+        }
+    }
+
+    pub fn init(&self, peripherals: &mut OptionalPeripherals) {
+        peripherals.EXTI0.take().unwrap();
+        peripherals.EXTI1.take().unwrap();
+        peripherals.EXTI2.take().unwrap();
+        peripherals.EXTI3.take().unwrap();
+        peripherals.EXTI4.take().unwrap();
+        peripherals.EXTI5.take().unwrap();
+        peripherals.EXTI6.take().unwrap();
+        peripherals.EXTI7.take().unwrap();
+        peripherals.EXTI8.take().unwrap();
+        peripherals.EXTI9.take().unwrap();
+        peripherals.EXTI10.take().unwrap();
+        peripherals.EXTI11.take().unwrap();
+        peripherals.EXTI12.take().unwrap();
+        peripherals.EXTI13.take().unwrap();
+        peripherals.EXTI14.take().unwrap();
+        peripherals.EXTI15.take().unwrap();
+
+        self.initialized.store(true, Ordering::Release);
+
+        // Do nothing else, just consume the peripherals: they are ours now!
+    }
+
+    pub fn get_interrupt_channel_for_pin<P: Peripheral<P = T>, T: Pin>(
+        &self,
+        pin: P,
+    ) -> Result<AnyChannel, InterruptError> {
+        // Make sure that the interrupt channels have been captured during initialization.
+        assert!(self.initialized.load(Ordering::Acquire));
+
+        let pin = pin.into_ref().map_into();
+        let pin_number = pin.pin();
+
+        // As interrupt channels are mutually exclusive between ports (ie., if channel i has
+        // been bound for pin i of a port, it cannot be used for pin i of another port), we
+        // only check the pin number.
+        // NOTE(ordering): since setting a bit is an idempotent operation, and since we do not
+        // allow clearing them, the ordering does not matter.
+        let was_used = self
+            .used_interrupt_channels
+            .bit_set(pin_number.into(), Ordering::Relaxed);
+
+        if was_used {
+            return Err(InterruptError::IntChannelAlreadyUsed);
+        }
+
+        // They are the same
+        let ch_number = pin_number;
+
+        // NOTE(embassy): ideally we would be using `T::ExtiChannel::steal()` instead of this
+        // match, but Embassy does not provide this.
+        // SAFETY: this function enforces that the same channel cannot be obtained twice,
+        // making sure multiple instances are not used at the same time as the mandatory
+        // `init()` method has collected all channel peripherals beforehand.
+        let ch = match ch_number {
+            0 => unsafe { peripherals::EXTI0::steal() }.degrade(),
+            1 => unsafe { peripherals::EXTI1::steal() }.degrade(),
+            2 => unsafe { peripherals::EXTI2::steal() }.degrade(),
+            3 => unsafe { peripherals::EXTI3::steal() }.degrade(),
+            4 => unsafe { peripherals::EXTI4::steal() }.degrade(),
+            5 => unsafe { peripherals::EXTI5::steal() }.degrade(),
+            6 => unsafe { peripherals::EXTI6::steal() }.degrade(),
+            7 => unsafe { peripherals::EXTI7::steal() }.degrade(),
+            8 => unsafe { peripherals::EXTI8::steal() }.degrade(),
+            9 => unsafe { peripherals::EXTI9::steal() }.degrade(),
+            10 => unsafe { peripherals::EXTI10::steal() }.degrade(),
+            11 => unsafe { peripherals::EXTI11::steal() }.degrade(),
+            12 => unsafe { peripherals::EXTI12::steal() }.degrade(),
+            13 => unsafe { peripherals::EXTI13::steal() }.degrade(),
+            14 => unsafe { peripherals::EXTI14::steal() }.degrade(),
+            15 => unsafe { peripherals::EXTI15::steal() }.degrade(),
+            _ => unreachable!(),
+        };
+
+        Ok(ch)
+    }
+}

src/riot-rs-stm32/src/gpio.rs
@@ -1,0 +1,165 @@
+pub trait IntoLevel {
+    fn into(level: Self) -> riot_rs_embassy_common::gpio::Level;
+}
+
+pub mod input {
+    use embassy_stm32::{
+        gpio::{Level, Pull},
+        Peripheral,
+    };
+
+    pub use embassy_stm32::gpio::{Input, Pin as InputPin};
+
+    #[cfg(feature = "external-interrupts")]
+    pub use embassy_stm32::exti::ExtiInput as IntEnabledInput;
+
+    pub const SCHMITT_TRIGGER_CONFIGURABLE: bool = false;
+
+    pub fn new(
+        pin: impl Peripheral<P: InputPin> + 'static,
+        pull: riot_rs_embassy_common::gpio::Pull,
+        _schmitt_trigger: bool, // Not supported by this architecture
+        // let pull = Pull::from(pull);
+    ) -> Result<Input<'static>, riot_rs_embassy_common::gpio::input::Error> {
+        let pull = from_pull(pull);
+        Ok(Input::new(pin, pull))
+    }
+
+    #[cfg(feature = "external-interrupts")]
+    pub fn new_int_enabled<P: Peripheral<P = T> + 'static, T: InputPin>(
+        pin: P,
+        pull: riot_rs_embassy_common::gpio::Pull,
+        _schmitt_trigger: bool, // Not supported by this architecture
+    ) -> Result<IntEnabledInput<'static>, riot_rs_embassy_common::gpio::input::Error> {
+        // let pull = Pull::from(pull);
+        let pull = from_pull(pull);
+        let mut pin = pin.into_ref();
+        let ch = crate::extint_registry::EXTINT_REGISTRY.get_interrupt_channel_for_pin(&mut pin)?;
+        let pin = pin.into_ref().map_into();
+        Ok(IntEnabledInput::new(pin, ch, pull))
+    }
+
+    impl crate::gpio::IntoLevel for Level {
+        fn into(level: Self) -> riot_rs_embassy_common::gpio::Level {
+            match level {
+                Level::Low => riot_rs_embassy_common::gpio::Level::Low,
+                Level::High => riot_rs_embassy_common::gpio::Level::High,
+            }
+        }
+    }
+
+    fn from_pull(pull: riot_rs_embassy_common::gpio::Pull) -> Pull {
+        match pull {
+            riot_rs_embassy_common::gpio::Pull::None => Pull::None,
+            riot_rs_embassy_common::gpio::Pull::Up => Pull::Up,
+            riot_rs_embassy_common::gpio::Pull::Down => Pull::Down,
+        }
+    }
+
+    // impl From<riot_rs_embassy_common::gpio::Pull> for Pull {
+    //     fn from(pull: riot_rs_embassy_common::gpio::Pull) -> Self {
+    //         match pull {
+    //             riot_rs_embassy_common::gpio::Pull::None => Pull::None,
+    //             riot_rs_embassy_common::gpio::Pull::Up => Pull::Up,
+    //             riot_rs_embassy_common::gpio::Pull::Down => Pull::Down,
+    //         }
+    //     }
+    // }
+    //
+    // impl From<Level> for riot_rs_embassy_common::gpio::Level {
+    //     fn from(level: Level) -> Self {
+    //         match level {
+    //             Level::Low => riot_rs_embassy_common::gpio::Level::Low,
+    //             Level::High => riot_rs_embassy_common::gpio::Level::High,
+    //         }
+    //     }
+    // }
+}
+
+pub mod output {
+    use embassy_stm32::{
+        gpio::{Level, Speed as StmSpeed},
+        Peripheral,
+    };
+
+    use riot_rs_embassy_common::gpio::{FromDriveStrength, FromSpeed};
+
+    pub use embassy_stm32::gpio::{Output, Pin as OutputPin};
+
+    pub const DRIVE_STRENGTH_CONFIGURABLE: bool = false;
+    pub const SPEED_CONFIGURABLE: bool = true;
+
+    pub fn new(
+        pin: impl Peripheral<P: OutputPin> + 'static,
+        initial_level: riot_rs_embassy_common::gpio::Level,
+        _drive_strength: DriveStrength, // Not supported by this architecture
+        speed: Speed,
+    ) -> Output<'static> {
+        let initial_level = match initial_level {
+            riot_rs_embassy_common::gpio::Level::Low => Level::Low,
+            riot_rs_embassy_common::gpio::Level::High => Level::High,
+        };
+        Output::new(pin, initial_level, speed.into())
+    }
+
+    // riot_rs_embassy_common::impl_from_level!(Level);
+
+    #[derive(Copy, Clone, PartialEq, Eq)]
+    pub enum DriveStrength {
+        UnsupportedByArchitecture,
+    }
+
+    impl Default for DriveStrength {
+        fn default() -> Self {
+            Self::UnsupportedByArchitecture
+        }
+    }
+
+    impl FromDriveStrength for DriveStrength {
+        fn from(drive_strength: riot_rs_embassy_common::gpio::DriveStrength<Self>) -> Self {
+            use riot_rs_embassy_common::gpio::DriveStrength::*;
+
+            match drive_strength {
+                Arch(drive_strength) => drive_strength,
+                Lowest => DriveStrength::UnsupportedByArchitecture,
+                Standard => DriveStrength::default(),
+                Medium => DriveStrength::UnsupportedByArchitecture,
+                High => DriveStrength::UnsupportedByArchitecture,
+                Highest => DriveStrength::UnsupportedByArchitecture,
+            }
+        }
+    }
+
+    #[derive(Copy, Clone, PartialEq, Eq)]
+    pub enum Speed {
+        Low,
+        Medium,
+        High,
+        VeryHigh,
+    }
+
+    impl From<Speed> for StmSpeed {
+        fn from(speed: Speed) -> Self {
+            match speed {
+                Speed::Low => StmSpeed::Low,
+                Speed::Medium => StmSpeed::Medium,
+                Speed::High => StmSpeed::High,
+                Speed::VeryHigh => StmSpeed::VeryHigh,
+            }
+        }
+    }
+
+    impl FromSpeed for Speed {
+        fn from(speed: riot_rs_embassy_common::gpio::Speed<Self>) -> Self {
+            use riot_rs_embassy_common::gpio::Speed::*;
+
+            match speed {
+                Arch(speed) => speed,
+                Low => Speed::Low,
+                Medium => Speed::Medium,
+                High => Speed::High,
+                VeryHigh => Speed::VeryHigh,
+            }
+        }
+    }
+}

src/riot-rs-stm32/src/usb_synopsis_otg.rs
@@ -1,6 +1,5 @@
 use embassy_stm32::{bind_interrupts, peripherals, usb, usb::Driver};
-
-use crate::{arch, ConstStaticCell};
+use static_cell::ConstStaticCell;
 
 bind_interrupts!(struct Irqs {
     OTG_FS => usb::InterruptHandler<peripherals::USB_OTG_FS>;
@@ -8,7 +7,7 @@ bind_interrupts!(struct Irqs {
 
 pub type UsbDriver = Driver<'static, peripherals::USB_OTG_FS>;
 
-pub fn driver(peripherals: &mut arch::OptionalPeripherals) -> UsbDriver {
+pub fn driver(peripherals: &mut crate::OptionalPeripherals) -> UsbDriver {
     let usb = peripherals.USB_OTG_FS.take().unwrap();
     let dp = peripherals.PA12.take().unwrap();
     let dm = peripherals.PA11.take().unwrap();
@@ -33,7 +32,7 @@ pub fn driver(peripherals: &mut arch::OptionalPeripherals) -> UsbDriver {
     #[cfg(feature = "executor-interrupt")]
     {
         use embassy_stm32::interrupt::{InterruptExt, Priority};
-        crate::arch::SWI.set_priority(Priority::P1);
+        crate::SWI.set_priority(Priority::P1);
         embassy_stm32::interrupt::OTG_FS.set_priority(Priority::P0);
     }
 