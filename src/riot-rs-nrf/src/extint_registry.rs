ROMemories/ariel-os

refactor(embassy)!: extract the nRF module into its own crate

src/riot-rs-nrf/src/extint_registry.rs
@@ -1,0 +1,49 @@
+use embassy_nrf::{gpio::Pin, Peripheral};
+use portable_atomic::{AtomicU8, Ordering};
+use riot_rs_shared_types::gpio::input::InterruptError;
+
+#[cfg(context = "nrf51")]
+const INT_CHANNEL_COUNT: u8 = 4;
+#[cfg(not(context = "nrf51"))]
+const INT_CHANNEL_COUNT: u8 = 8;
+
+pub static EXTINT_REGISTRY: ExtIntRegistry = ExtIntRegistry::new();
+
+pub struct ExtIntRegistry {
+    used_interrupt_channel_count: AtomicU8,
+}
+
+impl ExtIntRegistry {
+    #[expect(clippy::new_without_default)]
+    #[must_use]
+    pub const fn new() -> Self {
+        Self {
+            used_interrupt_channel_count: AtomicU8::new(0),
+        }
+    }
+
+    pub fn use_interrupt_for_pin<PIN: Peripheral<P: Pin>>(
+        &self,
+        _pin: &mut PIN, // Require the caller to have the peripheral
+    ) -> Result<(), InterruptError> {
+        // NOTE(ordering): this acts as a lock, so we use Acquire/Release ordering.
+        let update_res = self.used_interrupt_channel_count.fetch_update(
+            Ordering::AcqRel,
+            Ordering::Acquire,
+            |c| {
+                if c == INT_CHANNEL_COUNT {
+                    None
+                } else {
+                    // This cannot overflow because `INT_CHANNEL_COUNT` is lower than u8::MAX.
+                    Some(c + 1)
+                }
+            },
+        );
+
+        if update_res.is_err() {
+            return Err(InterruptError::NoIntChannelAvailable);
+        }
+
+        Ok(())
+    }
+}

src/riot-rs-nrf/src/gpio.rs
@@ -1,0 +1,146 @@
+pub trait IntoLevel {
+    fn into(level: Self) -> riot_rs_shared_types::gpio::Level;
+}
+
+pub mod input {
+    use embassy_nrf::{
+        gpio::{Level, Pull},
+        Peripheral,
+    };
+
+    pub use embassy_nrf::gpio::{Input, Pin as InputPin};
+
+    // Re-export `Input` as `IntEnabledInput` as they are interrupt-enabled.
+    #[cfg(feature = "external-interrupts")]
+    pub use embassy_nrf::gpio::Input as IntEnabledInput;
+
+    pub const SCHMITT_TRIGGER_CONFIGURABLE: bool = false;
+
+    pub fn new(
+        pin: impl Peripheral<P: InputPin> + 'static,
+        pull: riot_rs_shared_types::gpio::Pull,
+        _schmitt_trigger: bool, // Not supported by this architecture
+    ) -> Result<Input<'static>, riot_rs_shared_types::gpio::input::Error> {
+        let pull = from_pull(pull);
+        Ok(Input::new(pin, pull))
+    }
+
+    #[cfg(feature = "external-interrupts")]
+    pub fn new_int_enabled(
+        pin: impl Peripheral<P: InputPin> + 'static,
+        pull: riot_rs_shared_types::gpio::Pull,
+        _schmitt_trigger: bool, // Not supported by this architecture
+    ) -> Result<IntEnabledInput<'static>, riot_rs_shared_types::gpio::input::Error> {
+        let pull = from_pull(pull);
+        let mut pin = pin.into_ref();
+        crate::extint_registry::EXTINT_REGISTRY.use_interrupt_for_pin(&mut pin)?;
+        Ok(Input::new(pin, pull))
+    }
+
+    fn from_pull(pull: riot_rs_shared_types::gpio::Pull) -> Pull {
+        match pull {
+            riot_rs_shared_types::gpio::Pull::None => Pull::None,
+            riot_rs_shared_types::gpio::Pull::Up => Pull::Up,
+            riot_rs_shared_types::gpio::Pull::Down => Pull::Down,
+        }
+    }
+
+    impl crate::gpio::IntoLevel for Level {
+        fn into(level: Self) -> riot_rs_shared_types::gpio::Level {
+            match level {
+                Level::Low => riot_rs_shared_types::gpio::Level::Low,
+                Level::High => riot_rs_shared_types::gpio::Level::High,
+            }
+        }
+    }
+
+    // impl From<crate::gpio::Pull> for Pull {
+    //     fn from(pull: crate::gpio::Pull) -> Self {
+    //         match pull {
+    //             crate::gpio::Pull::None => Pull::None,
+    //             crate::gpio::Pull::Up => Pull::Up,
+    //             crate::gpio::Pull::Down => Pull::Down,
+    //         }
+    //     }
+    // }
+    //
+    // impl From<Level> for crate::gpio::Level {
+    //     fn from(level: Level) -> Self {
+    //         match level {
+    //             Level::Low => crate::gpio::Level::Low,
+    //             Level::High => crate::gpio::Level::High,
+    //         }
+    //     }
+    // }
+}
+
+pub mod output {
+    use embassy_nrf::{
+        gpio::{Level, OutputDrive},
+        Peripheral,
+    };
+    use riot_rs_shared_types::gpio::{FromDriveStrength, FromSpeed};
+
+    pub use embassy_nrf::gpio::{Output, Pin as OutputPin};
+
+    pub const DRIVE_STRENGTH_CONFIGURABLE: bool = true;
+    pub const SPEED_CONFIGURABLE: bool = false;
+
+    pub fn new(
+        pin: impl Peripheral<P: OutputPin> + 'static,
+        initial_level: riot_rs_shared_types::gpio::Level,
+        drive_strength: DriveStrength,
+        _speed: Speed, // Not supported by this architecture
+    ) -> Output<'static> {
+        let output_drive = match drive_strength {
+            DriveStrength::Standard => OutputDrive::Standard,
+            DriveStrength::High => OutputDrive::HighDrive,
+        };
+        let initial_level = match initial_level {
+            riot_rs_shared_types::gpio::Level::Low => Level::Low,
+            riot_rs_shared_types::gpio::Level::High => Level::High,
+        };
+        Output::new(pin, initial_level, output_drive)
+    }
+
+    // crate::gpio::impl_from_level!(Level);
+
+    #[derive(Copy, Clone, PartialEq, Eq)]
+    pub enum DriveStrength {
+        Standard,
+        High, // Around 10 mA
+    }
+
+    impl Default for DriveStrength {
+        fn default() -> Self {
+            Self::Standard
+        }
+    }
+
+    impl FromDriveStrength for DriveStrength {
+        fn from(drive_strength: riot_rs_shared_types::gpio::DriveStrength<Self>) -> Self {
+            use riot_rs_shared_types::gpio::DriveStrength::*;
+
+            // ESPs are able to output up to 40 mA, so we somewhat normalize this.
+            match drive_strength {
+                Arch(drive_strength) => drive_strength,
+                Lowest => DriveStrength::Standard,
+                Standard => DriveStrength::default(),
+                Medium => DriveStrength::Standard,
+                High => DriveStrength::High,
+                Highest => DriveStrength::High,
+            }
+        }
+    }
+
+    #[derive(Copy, Clone, PartialEq, Eq)]
+    pub enum Speed {
+        UnsupportedByArchitecture,
+    }
+
+    impl FromSpeed for Speed {
+        fn from(_speed: riot_rs_shared_types::gpio::Speed<Self>) -> Self {
+            Self::UnsupportedByArchitecture
+        }
+    }
+}

src/riot-rs-nrf/src/lib.rs
@@ -1,8 +1,15 @@
+#![no_std]
+#![feature(doc_auto_cfg)]
+#![feature(lint_reasons)]
+
 pub mod gpio;
 
 pub mod peripheral {
     pub use embassy_nrf::Peripheral;
 }
+
+#[cfg(feature = "external-interrupts")]
+pub mod extint_registry;
 
 #[cfg(feature = "hwrng")]
 pub mod hwrng;
@@ -11,19 +18,22 @@ pub mod hwrng;
 pub mod usb;
 
 #[cfg(feature = "executor-interrupt")]
-pub(crate) use embassy_executor::InterruptExecutor as Executor;
+pub use embassy_executor::InterruptExecutor as Executor;
 
 #[cfg(feature = "executor-interrupt")]
 #[cfg(context = "nrf52")]
-crate::executor_swi!(SWI0_EGU0);
+riot_rs_shared_types::executor_swi!(SWI0_EGU0);
 
 #[cfg(feature = "executor-interrupt")]
 #[cfg(context = "nrf5340")]
-crate::executor_swi!(EGU0);
+riot_rs_shared_types::executor_swi!(EGU0);
 
 use embassy_nrf::config::Config;
 
 pub use embassy_nrf::{interrupt, peripherals, OptionalPeripherals};
+
+#[cfg(feature = "executor-interrupt")]
+pub static EXECUTOR: Executor = Executor::new();
 
 pub fn init() -> OptionalPeripherals {
     let peripherals = embassy_nrf::init(Config::default());

future-proof-iot/RIOT-rs

src/riot-rs-nrf/src/extint_registry.rs
@@ -1,0 +1,49 @@
+use embassy_nrf::{gpio::Pin, Peripheral};
+use portable_atomic::{AtomicU8, Ordering};
+use riot_rs_embassy_common::gpio::input::InterruptError;
+
+#[cfg(context = "nrf51")]
+const INT_CHANNEL_COUNT: u8 = 4;
+#[cfg(not(context = "nrf51"))]
+const INT_CHANNEL_COUNT: u8 = 8;
+
+pub static EXTINT_REGISTRY: ExtIntRegistry = ExtIntRegistry::new();
+
+pub struct ExtIntRegistry {
+    used_interrupt_channel_count: AtomicU8,
+}
+
+impl ExtIntRegistry {
+    #[expect(clippy::new_without_default)]
+    #[must_use]
+    pub const fn new() -> Self {
+        Self {
+            used_interrupt_channel_count: AtomicU8::new(0),
+        }
+    }
+
+    pub fn use_interrupt_for_pin<PIN: Peripheral<P: Pin>>(
+        &self,
+        _pin: &mut PIN, // Require the caller to have the peripheral
+    ) -> Result<(), InterruptError> {
+        // NOTE(ordering): this acts as a lock, so we use Acquire/Release ordering.
+        let update_res = self.used_interrupt_channel_count.fetch_update(
+            Ordering::AcqRel,
+            Ordering::Acquire,
+            |c| {
+                if c == INT_CHANNEL_COUNT {
+                    None
+                } else {
+                    // This cannot overflow because `INT_CHANNEL_COUNT` is lower than u8::MAX.
+                    Some(c + 1)
+                }
+            },
+        );
+
+        if update_res.is_err() {
+            return Err(InterruptError::NoIntChannelAvailable);
+        }
+
+        Ok(())
+    }
+}

src/riot-rs-nrf/src/gpio.rs
@@ -1,0 +1,146 @@
+pub trait IntoLevel {
+    fn into(level: Self) -> riot_rs_embassy_common::gpio::Level;
+}
+
+pub mod input {
+    use embassy_nrf::{
+        gpio::{Level, Pull},
+        Peripheral,
+    };
+
+    pub use embassy_nrf::gpio::{Input, Pin as InputPin};
+
+    // Re-export `Input` as `IntEnabledInput` as they are interrupt-enabled.
+    #[cfg(feature = "external-interrupts")]
+    pub use embassy_nrf::gpio::Input as IntEnabledInput;
+
+    pub const SCHMITT_TRIGGER_CONFIGURABLE: bool = false;
+
+    pub fn new(
+        pin: impl Peripheral<P: InputPin> + 'static,
+        pull: riot_rs_embassy_common::gpio::Pull,
+        _schmitt_trigger: bool, // Not supported by this architecture
+    ) -> Result<Input<'static>, riot_rs_embassy_common::gpio::input::Error> {
+        let pull = from_pull(pull);
+        Ok(Input::new(pin, pull))
+    }
+
+    #[cfg(feature = "external-interrupts")]
+    pub fn new_int_enabled(
+        pin: impl Peripheral<P: InputPin> + 'static,
+        pull: riot_rs_embassy_common::gpio::Pull,
+        _schmitt_trigger: bool, // Not supported by this architecture
+    ) -> Result<IntEnabledInput<'static>, riot_rs_embassy_common::gpio::input::Error> {
+        let pull = from_pull(pull);
+        let mut pin = pin.into_ref();
+        crate::extint_registry::EXTINT_REGISTRY.use_interrupt_for_pin(&mut pin)?;
+        Ok(Input::new(pin, pull))
+    }
+
+    fn from_pull(pull: riot_rs_embassy_common::gpio::Pull) -> Pull {
+        match pull {
+            riot_rs_embassy_common::gpio::Pull::None => Pull::None,
+            riot_rs_embassy_common::gpio::Pull::Up => Pull::Up,
+            riot_rs_embassy_common::gpio::Pull::Down => Pull::Down,
+        }
+    }
+
+    impl crate::gpio::IntoLevel for Level {
+        fn into(level: Self) -> riot_rs_embassy_common::gpio::Level {
+            match level {
+                Level::Low => riot_rs_embassy_common::gpio::Level::Low,
+                Level::High => riot_rs_embassy_common::gpio::Level::High,
+            }
+        }
+    }
+
+    // impl From<crate::gpio::Pull> for Pull {
+    //     fn from(pull: crate::gpio::Pull) -> Self {
+    //         match pull {
+    //             crate::gpio::Pull::None => Pull::None,
+    //             crate::gpio::Pull::Up => Pull::Up,
+    //             crate::gpio::Pull::Down => Pull::Down,
+    //         }
+    //     }
+    // }
+    //
+    // impl From<Level> for crate::gpio::Level {
+    //     fn from(level: Level) -> Self {
+    //         match level {
+    //             Level::Low => crate::gpio::Level::Low,
+    //             Level::High => crate::gpio::Level::High,
+    //         }
+    //     }
+    // }
+}
+
+pub mod output {
+    use embassy_nrf::{
+        gpio::{Level, OutputDrive},
+        Peripheral,
+    };
+    use riot_rs_embassy_common::gpio::{FromDriveStrength, FromSpeed};
+
+    pub use embassy_nrf::gpio::{Output, Pin as OutputPin};
+
+    pub const DRIVE_STRENGTH_CONFIGURABLE: bool = true;
+    pub const SPEED_CONFIGURABLE: bool = false;
+
+    pub fn new(
+        pin: impl Peripheral<P: OutputPin> + 'static,
+        initial_level: riot_rs_embassy_common::gpio::Level,
+        drive_strength: DriveStrength,
+        _speed: Speed, // Not supported by this architecture
+    ) -> Output<'static> {
+        let output_drive = match drive_strength {
+            DriveStrength::Standard => OutputDrive::Standard,
+            DriveStrength::High => OutputDrive::HighDrive,
+        };
+        let initial_level = match initial_level {
+            riot_rs_embassy_common::gpio::Level::Low => Level::Low,
+            riot_rs_embassy_common::gpio::Level::High => Level::High,
+        };
+        Output::new(pin, initial_level, output_drive)
+    }
+
+    // crate::gpio::impl_from_level!(Level);
+
+    #[derive(Copy, Clone, PartialEq, Eq)]
+    pub enum DriveStrength {
+        Standard,
+        High, // Around 10 mA
+    }
+
+    impl Default for DriveStrength {
+        fn default() -> Self {
+            Self::Standard
+        }
+    }
+
+    impl FromDriveStrength for DriveStrength {
+        fn from(drive_strength: riot_rs_embassy_common::gpio::DriveStrength<Self>) -> Self {
+            use riot_rs_embassy_common::gpio::DriveStrength::*;
+
+            // ESPs are able to output up to 40 mA, so we somewhat normalize this.
+            match drive_strength {
+                Arch(drive_strength) => drive_strength,
+                Lowest => DriveStrength::Standard,
+                Standard => DriveStrength::default(),
+                Medium => DriveStrength::Standard,
+                High => DriveStrength::High,
+                Highest => DriveStrength::High,
+            }
+        }
+    }
+
+    #[derive(Copy, Clone, PartialEq, Eq)]
+    pub enum Speed {
+        UnsupportedByArchitecture,
+    }
+
+    impl FromSpeed for Speed {
+        fn from(_speed: riot_rs_embassy_common::gpio::Speed<Self>) -> Self {
+            Self::UnsupportedByArchitecture
+        }
+    }
+}

src/riot-rs-nrf/src/lib.rs
@@ -1,8 +1,15 @@
+#![no_std]
+#![feature(doc_auto_cfg)]
+#![feature(lint_reasons)]
+
 pub mod gpio;
 
 pub mod peripheral {
     pub use embassy_nrf::Peripheral;
 }
+
+#[cfg(feature = "external-interrupts")]
+pub mod extint_registry;
 
 #[cfg(feature = "hwrng")]
 pub mod hwrng;
@@ -11,19 +18,22 @@ pub mod hwrng;
 pub mod usb;
 
 #[cfg(feature = "executor-interrupt")]
-pub(crate) use embassy_executor::InterruptExecutor as Executor;
+pub use embassy_executor::InterruptExecutor as Executor;
 
 #[cfg(feature = "executor-interrupt")]
 #[cfg(context = "nrf52")]
-crate::executor_swi!(SWI0_EGU0);
+riot_rs_embassy_common::executor_swi!(SWI0_EGU0);
 
 #[cfg(feature = "executor-interrupt")]
 #[cfg(context = "nrf5340")]
-crate::executor_swi!(EGU0);
+riot_rs_embassy_common::executor_swi!(EGU0);
 
 use embassy_nrf::config::Config;
 
 pub use embassy_nrf::{interrupt, peripherals, OptionalPeripherals};
+
+#[cfg(feature = "executor-interrupt")]
+pub static EXECUTOR: Executor = Executor::new();
 
 pub fn init() -> OptionalPeripherals {
     let peripherals = embassy_nrf::init(Config::default());