075506c5e97f5c3b57da02a4cb6718c14d5ff226
Kaspar Schleiser <kaspar@schleiser.de>
2024-09-19 13:54:09 +0200

    feat(examples): bump picoserve to 0.12.x

--- examples/embassy-http-server/src/routes/index.rs
+++
@@ -1,8 +0,0 @@
-use picoserve::response::IntoResponse;
-
-pub async fn index() -> impl IntoResponse {
-    picoserve::response::File::html(include_str!(concat!(
-        env!("CARGO_MANIFEST_DIR"),
-        "/static/index.html",
-    )))
-}
