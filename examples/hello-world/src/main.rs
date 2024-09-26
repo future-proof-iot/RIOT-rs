da1b92bf475a79c0b0dae56e647dafa283be536b
chrysn <chrysn@fsfe.org>
2024-09-27 00:30:30 +0200

    feat(examples): Show device ID next to board in hello-world

--- examples/hello-world/src/main.rs
+++ examples/hello-world/src/main.rs
@@ -7,8 +7,9 @@ use riot_rs::debug::{exit, log::*};
 #[riot_rs::thread(autostart)]
 fn main() {
     info!(
-        "Hello from main()! Running on a {} board.",
+        "Hello from main()! Running on {} board identified as {:x}.",
         riot_rs::buildinfo::BOARD,
+        riot_rs::arch::identity::device_id(),
     );
 
     exit(Ok(()));
