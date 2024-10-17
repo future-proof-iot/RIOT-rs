edeaddbdf214ef23fca1850cf1d1dd19b0dbdb87
Elena Frank <elena.frank@protonmail.com>
2024-10-17 10:46:01 +0200

    fixup! feat(examples): add threading with multicore example
    Simplify example.

--- examples/threading-multicore/src/main.rs
+++ examples/threading-multicore/src/main.rs
@@ -5,50 +5,21 @@
 
 use riot_rs::{
     debug::log::*,
-    thread::{sync::Channel, yield_same, CoreAffinity, CoreId},
+    thread::{CoreAffinity, CoreId},
 };
 
-static CHANNEL: Channel<bool> = Channel::new();
-
-#[riot_rs::thread(autostart, priority = 1)]
+#[riot_rs::thread(autostart, affinity = CoreAffinity::one(CoreId::new(1)))]
 fn thread0() {
     let core = riot_rs::thread::core_id();
     let pid = riot_rs::thread::current_pid().unwrap();
-    info!("Hello from low prio thread {:?} on core {:?}", pid, core);
+    info!("Hello from {:?} on {:?}", pid, core);
     loop {}
 }
 
-#[riot_rs::thread(autostart, priority = 2)]
+#[riot_rs::thread(autostart)]
 fn thread1() {
     let core = riot_rs::thread::core_id();
     let pid = riot_rs::thread::current_pid().unwrap();
-    info!("Hello from mid prio thread {:?} on core {:?}", pid, core);
-    yield_same();
-    info!("{:?} running again on core {:?}", pid, core);
-}
-
-#[riot_rs::thread(autostart, priority = 2)]
-fn thread2() {
-    let core = riot_rs::thread::core_id();
-    let pid = riot_rs::thread::current_pid().unwrap();
-    info!("Hello from mid prio thread {:?} on core {:?}", pid, core);
-    yield_same();
-    CHANNEL.send(&true);
-    info!("{:?} running again (forever) on core {:?}", pid, core);
+    info!("Hello from {:?} on {:?}", pid, core);
     loop {}
-}
-
-#[riot_rs::thread(autostart, priority = 3, affinity = CoreAffinity::one(CoreId::new(1)))]
-fn thread3() {
-    let core = riot_rs::thread::core_id();
-    let pid = riot_rs::thread::current_pid().unwrap();
-    info!("Hello from high prio thread {:?} on core {:?}", pid, core);
-}
-
-#[riot_rs::thread(autostart, priority = 3, affinity = CoreAffinity::one(CoreId::new(1)))]
-fn thread4() {
-    let core = riot_rs::thread::core_id();
-    let pid = riot_rs::thread::current_pid().unwrap();
-    info!("Hello from high prio thread {:?} on core {:?}", pid, core);
-    while CHANNEL.try_recv().is_none() {}
 }
