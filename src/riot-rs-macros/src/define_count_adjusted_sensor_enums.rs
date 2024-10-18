a23c4dc9adba518c63855fad196a1081b36f29ba
ROMemories <152802150+ROMemories@users.noreply.github.com>
2024-10-18 15:21:02 +0200

    fixup! feat: introduce a sensor abstraction

--- src/riot-rs-macros/src/define_count_adjusted_sensor_enums.rs
+++ src/riot-rs-macros/src/define_count_adjusted_sensor_enums.rs
@@ -1,3 +1,9 @@
+/// Generates sensor-related enums whose number of variants needs to be adjusted based on Cargo
+/// features, to accommodate the sensor driver returning the largest number of values.
+///
+/// One single type must be defined so that it can be used in the Future returned by sensor
+/// drivers, which must be the same for every sensor driver so it can be part of the `Sensor`
+/// trait.
 #[proc_macro]
 pub fn define_count_adjusted_sensor_enums(_item: TokenStream) -> TokenStream {
     use quote::quote;
