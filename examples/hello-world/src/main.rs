8a02882b4258236077ace8ef69ccdf10422bef79
chrysn <chrysn@fsfe.org>
2024-10-18 11:19:04 +0200

    squashgit add . Move device-id example into dedicated example

--- examples/hello-world/src/main.rs
+++ examples/hello-world/src/main.rs
@@ -7,12 +7,8 @@ use riot_rs::debug::{exit, log::*};
 #[riot_rs::thread(autostart)]
 fn main() {
     info!(
-        "Hello from main()! Running on {} board identified as {:x}.",
+        "Hello from main()! Running on a {} board.",
         riot_rs::buildinfo::BOARD,
-        riot_rs::identity::device_id_bytes()
-            .ok()
-            .as_ref()
-            .map(|b| b.as_ref()),
     );
 
     exit(Ok(()));
