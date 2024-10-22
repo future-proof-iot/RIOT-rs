7cb1e4f3e127a99096ca51414eb4e5cabbc44e38
Kaspar Schleiser <kaspar@schleiser.de>
2024-10-22 10:53:10 +0200

    fix(examples): make `hello-world-async` exit

--- examples/hello-world-async/src/main.rs
+++ examples/hello-world-async/src/main.rs
@@ -3,9 +3,11 @@
 #![feature(impl_trait_in_assoc_type)]
 #![feature(used_with_arg)]
 
-use riot_rs::debug::log::*;
+use riot_rs::debug::{exit, log::*};
 
 #[riot_rs::task(autostart)]
 async fn main() {
     info!("Hello World!");
+
+    exit(Ok(()));
 }
