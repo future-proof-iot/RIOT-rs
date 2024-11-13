168c11a5f0d45f7a63f4a2b7110edb65baa91da1
ROMemories <152802150+ROMemories@users.noreply.github.com>
2024-11-13 17:13:42 +0100

    docs(hal): document where items from the `hal` mod are coming from

--- src/ariel-os-hal/src/dummy/mod.rs
+++ src/ariel-os-hal/src/dummy/mod.rs
@@ -1,42 +1,51 @@
 //! Dummy module used to satisfy platform-independent tooling.
-// TODO: redirect to the manufacturer-specific crate documentation when we publish it, and
-// mark every item in this dummy module `doc(hidden)`
 
 mod executor;
+
+#[doc(hidden)]
 pub mod gpio;
 
+#[doc(hidden)]
 pub mod peripheral {
     pub use embassy_hal_internal::Peripheral;
 }
 
+#[doc(hidden)]
 #[cfg(feature = "hwrng")]
 pub mod hwrng;
 
+#[doc(hidden)]
 #[cfg(feature = "i2c")]
 pub mod i2c;
 
+#[doc(hidden)]
 pub mod identity {
     use ariel_os_embassy_common::identity;
 
     pub type DeviceId = identity::NoDeviceId<identity::NotImplemented>;
 }
 
+#[doc(hidden)]
 #[cfg(feature = "spi")]
 pub mod spi;
 
+#[doc(hidden)]
 #[cfg(feature = "storage")]
 pub mod storage;
 
+#[doc(hidden)]
 #[cfg(feature = "usb")]
 pub mod usb;
 
 pub use executor::{Executor, Spawner};
 
+#[doc(hidden)]
 /// Dummy type.
 ///
 /// See the `OptionalPeripherals` type of your Embassy HAL crate instead.
 pub struct OptionalPeripherals;
 
+#[doc(hidden)]
 /// Dummy type.
 pub struct Peripherals;
 
@@ -46,8 +55,10 @@ impl From<Peripherals> for OptionalPeripherals {
     }
 }
 
+#[doc(hidden)]
 pub fn init() -> OptionalPeripherals {
     unimplemented!();
 }
 
+#[doc(hidden)]
 pub struct SWI;
--- src/ariel-os-hal/src/lib.rs
+++ src/ariel-os-hal/src/lib.rs
@@ -1,4 +1,19 @@
-//! This module dispatches between the ariel-os HAL crates.
+//! Provides MCU-specific items.
+//!
+//! This module dispatches between one of the following crate, depending on the target MCU family:
+//!
+//! | Manufacturer         | MCU family  | Docs rendered for | Items imported                                       |
+//! | -------------------- | ----------- | ----------------- | ---------------------------------------------------- |
+//! | Espressif            | ESP32       | ESP32-C6          | [`ariel-os-esp::*`](../../ariel_os_esp/index.html)     |
+//! | Nordic Semiconductor | nRF         | nRF52840          | [`ariel-os-nrf::*`](../../ariel_os_nrf/index.html)     |
+//! | Raspberry Pi         | RP          | RP2040            | [`ariel-os-rp::*`](../../ariel_os_rp/index.html)       |
+//! | STMicroelectronics   | STM32       | STM32W55RGVX      | [`ariel-os-stm32::*`](../../ariel_os_stm32/index.html) |
+//!
+//! Documentation is only rendered for the MCUs listed in the table above, but [many others are
+//! supported](https://ariel-os.github.io/ariel-os/dev/docs/book/hardware_functionality_support.html).
+//! To render the docs locally for the MCU of your choice, adapt [the `cargo doc` command used to
+//! generate documentation for the relevant
+//! crate](https://github.com/ariel-os/ariel-os/blob/main/.github/workflows/build-deploy-docs.yml).
 
 #![no_std]
 
