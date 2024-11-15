2bc65f1874bcc8692a9197b7f50fdc02085fddcf
Kaspar Schleiser <kaspar@schleiser.de>
2024-11-15 11:56:29 +0100

    fix(macros): update for changed `Spawner` import

--- src/riot-rs-macros/tests/ui/spawner/missing_peripherals_param.rs
+++ src/riot-rs-macros/tests/ui/spawner/missing_peripherals_param.rs
@@ -3,7 +3,7 @@
 
 // As the macro will fail, this import will not get used
 #[allow(unused_imports)]
-use riot_rs::Spawner;
+use riot_rs::asynch::Spawner;
 
 // FAIL: the `peripherals` parameter is required in this case
 #[riot_rs::spawner(autostart)]
