5a6a33b48ffa7be1021c6c4ed4fbe7391a1243d1
Kaspar Schleiser <kaspar@schleiser.de>
2024-11-20 16:42:34 +0100

    chore!: 's/RIOT_RS/ARIEL_OS/g'

--- src/ariel-os-macros/src/utils.rs
+++ src/ariel-os-macros/src/utils.rs
@@ -1,6 +1,6 @@
 use quote::format_ident;
 
-const RIOT_RS_CRATE_NAME: &str = "ariel-os";
+const ARIEL_OS_CRATE_NAME: &str = "ariel-os";
 
 /// Returns a [`struct@syn::Ident`] identifying the `ariel-os` dependency.
 ///
@@ -10,8 +10,8 @@ const RIOT_RS_CRATE_NAME: &str = "ariel-os";
 ///   this function is called.
 /// - Panics if `ariel-os` is used as a dependency of itself.
 pub fn riot_rs_crate() -> syn::Ident {
-    find_crate(RIOT_RS_CRATE_NAME)
-        .unwrap_or_else(|| panic!("{RIOT_RS_CRATE_NAME} should be present in `Cargo.toml`"))
+    find_crate(ARIEL_OS_CRATE_NAME)
+        .unwrap_or_else(|| panic!("{ARIEL_OS_CRATE_NAME} should be present in `Cargo.toml`"))
 }
 
 /// Returns a [`struct@syn::Ident`] identifying the `name` dependency (or `None`).
