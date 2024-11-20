999e6ebf38a5c9c90fa0b0e88dbdfe091aaca985
Kaspar Schleiser <kaspar@schleiser.de>
2024-11-20 11:04:44 +0100

    fix(esp): disable `usb-ethernet` for `esp32s3`

--- src/riot-rs-esp/src/usb/mod.rs
+++ src/riot-rs-esp/src/usb/mod.rs
@@ -1,16 +1,30 @@
 use esp_hal::otg_fs::asynch::Driver;
 use static_cell::ConstStaticCell;
 
+use crate::peripherals;
+
 pub type UsbDriver = Driver<'static>;
 
-pub fn driver(peripherals: &mut crate::OptionalPeripherals) -> UsbDriver {
+pub struct Peripherals {
+    usb: peripherals::USB0,
+    usbdp: peripherals::GPIO_20,
+    usbdm: peripherals::GPIO_19,
+}
+
+impl Peripherals {
+    #[must_use]
+    pub fn new(peripherals: &mut crate::OptionalPeripherals) -> Self {
+        Self {
+            usb: peripherals.USB0.take().unwrap(),
+            usbdp: peripherals.GPIO_20.take().unwrap(),
+            usbdm: peripherals.GPIO_19.take().unwrap(),
+        }
+    }
+}
+pub fn driver(peripherals: Peripherals) -> UsbDriver {
     use esp_hal::otg_fs::{asynch::Config, Usb};
 
-    let usb = Usb::new(
-        peripherals.USB0.take().unwrap(),
-        peripherals.GPIO_20.take().unwrap(),
-        peripherals.GPIO_19.take().unwrap(),
-    );
+    let usb = Usb::new(peripherals.usb, peripherals.usbdp, peripherals.usbdm);
 
     // Buffer size copied from upstream. There's no hint about sizing.
     static EP_OUT_BUFFER: ConstStaticCell<[u8; 1024]> = ConstStaticCell::new([0u8; 1024]);
